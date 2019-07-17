ea4a2a156ec8385dd842549bf123e07d7e7dcec0
Braden Hancock <hancock.braden@gmail.com>
2019-07-17 16:44:25 -0700

    Fix mdkir

--- spam/download_data.sh
+++ spam/download_data.sh
@@ -5,7 +5,7 @@ DATA_URL="https://archive.ics.uci.edu/ml/machine-learning-databases/00380/YouTub
 
 cd spam
 mkdir -p data
-mdkir -p raw_data
+mkdir -p raw_data
 wget $DATA_URL -O data.zip
 mv data.zip raw_data/
 cd raw_data
