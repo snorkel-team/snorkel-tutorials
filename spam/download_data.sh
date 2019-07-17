c289fe9d110e24bb8947f32a3991ba91a8e47b52
Braden Hancock <hancock.braden@gmail.com>
2019-07-17 13:28:18 -0700

    Create download scripts for spam

---
+++ spam/download_data.sh
@@ -0,0 +1,14 @@
+# Execute from root of snorkel-tutorials/
+# Download data,
+
+DATA_URL="https://archive.ics.uci.edu/ml/machine-learning-databases/00380/YouTube-Spam-Collection-v1.zip"
+
+cd spam
+mkdir -p data
+mdkir -p raw_data
+wget $DATA_URL -O data.zip
+mv data.zip raw_data/
+cd raw_data
+unzip data.zip
+rm data.zip
+rm -rf __MACOSX
---
+++ spam/utils.py
@@ -0,0 +1,33 @@
+import glob
+import os
+import subprocess
+import sys
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def load_spam_dataset():
+    # TODO:
+    # Add reference to dataset
+    # Send email to dataset owner: tuliocasagrande < AT > acm.org
+    subprocess.call(["bash", "download_data.sh"], shell=True)
+    filenames = sorted(glob.glob("raw_data/Youtube*.csv"))
+
+    dfs = []
+    for i, filename in enumerate(filenames, start=1):
+        df = pd.read_csv(filename)
+        df["VIDEO_ID"] = [i] * len(df)
+        df = df.rename(columns={"CLASS": "LABEL"})
+        df['LABEL'] = df['LABEL'].map({0: 2, 1: 1})
+        dfs.append(df)
+
+    df_train = pd.concat(dfs[:4])
+    df_valid, df_test = train_test_split(dfs[4], test_size=0.5)
+
+    # TODO: Drop the label column for train
+    return df_train, df_valid, df_test
+
+
+if __name__ == "__main__":
+    load_spam_dataset()
