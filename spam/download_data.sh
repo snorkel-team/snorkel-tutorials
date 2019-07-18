438a8b7c4605989e0f28e088f24e62edbd40eb7e
Braden Hancock <hancock.braden@gmail.com>
2019-07-18 12:10:55 -0700

    Don't create data/ directory

--- spam/download_data.sh
+++ spam/download_data.sh
@@ -4,7 +4,6 @@
 DATA_URL="https://archive.ics.uci.edu/ml/machine-learning-databases/00380/YouTube-Spam-Collection-v1.zip"
 
 cd spam
-mkdir -p data
 mkdir -p raw_data
 wget $DATA_URL -O data.zip
 mv data.zip raw_data/
