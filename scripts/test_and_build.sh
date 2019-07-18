ebfcccff2e6f2dce47c79a6071799e618c941000
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-07-18 17:33:39 +0000

    Handle errors

--- scripts/test_and_build.sh
+++ scripts/test_and_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -e 
+set -ex
 set -o pipefail
 
 SCRIPT_DIR=$(dirname "$0")
 BUILD_DIR=($SCRIPT_DIR/../build)
 TUTORIAL_DIR=$1
 
-python $SCRIPT_DIR/check_notebooks.py $TUTORIAL_DIR || 
+python $SCRIPT_DIR/check_notebooks.py $TUTORIAL_DIR
 mkdir -p $BUILD_DIR
 jupyter nbconvert $TUTORIAL_DIR/*.ipynb --to html --output-dir $BUILD_DIR
