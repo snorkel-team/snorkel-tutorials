b90f050dce18bad698a8db44ef342dd2dd04c7f5
Braden Hancock <hancock.braden@gmail.com>
2019-07-18 12:11:05 -0700

    Shuffle data and make four splits

--- spam/utils.py
+++ spam/utils.py
@@ -19,19 +19,27 @@ def load_spam_dataset():
         df = pd.read_csv(filename)
         df["VIDEO_ID"] = [i] * len(df)
         df = df.rename(columns={"CLASS": "LABEL"})
+        df = df.sample(frac=1).reset_index(drop=True)  # Shuffle order
         df['LABEL'] = df['LABEL'].map({0: 2, 1: 1})
         dfs.append(df)
 
-    df_train = pd.concat(dfs[:4])
+    df_train_dev = pd.concat(dfs[:4])
+    df_train, df_dev = train_test_split(
+        df_train_dev,
+        test_size=0.1,
+        random_state=123,
+        stratify=df_train_dev.LABEL
+    )
+    df_valid_test = dfs[4]
     df_valid, df_test = train_test_split(
-        dfs[4],
+        df_valid_test,
         test_size=0.5,
         random_state=123,
-        stratify=dfs[4].LABEL,
+        stratify=df_valid_test.LABEL,
     )
 
     # TODO: Drop the label column for train
-    return df_train, df_valid, df_test
+    return df_train, df_dev, df_valid, df_test
 
 
 if __name__ == "__main__":
