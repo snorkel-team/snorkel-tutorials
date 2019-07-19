bf64bd859b256a8e0aba3b40f893b61902a9647e
Manas Joglekar <brahmaneya@gmail.com>
2019-07-19 23:26:35 +0000

    Removed header

--- spouse/spouse_demo.py
+++ spouse/spouse_demo.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:percent
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.2'
-#       jupytext_version: 1.1.7
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
 # %% [markdown]
 # # Detecting spouse mentions in sentences
 
