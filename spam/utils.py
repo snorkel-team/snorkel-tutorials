8aebe127be019f95249b319f0ef75af884ba94e8
Braden Hancock <hancock.braden@gmail.com>
2019-07-18 21:16:29 -0700

    Save full spam_tutorial outline

--- spam/utils.py
+++ spam/utils.py
@@ -11,7 +11,7 @@ def load_spam_dataset():
     # TODO:
     # Add reference to dataset
     # Send email to dataset owner: tuliocasagrande < AT > acm.org
-    subprocess.call(["bash", "download_data.sh"], shell=True)
+    subprocess.call("bash download_data.sh", shell=True)
     filenames = sorted(glob.glob("raw_data/Youtube*.csv"))
 
     dfs = []
@@ -23,13 +23,8 @@ def load_spam_dataset():
         df['LABEL'] = df['LABEL'].map({0: 2, 1: 1})
         dfs.append(df)
 
-    df_train_dev = pd.concat(dfs[:4])
-    df_train, df_dev = train_test_split(
-        df_train_dev,
-        test_size=0.1,
-        random_state=123,
-        stratify=df_train_dev.LABEL
-    )
+    df_train = pd.concat(dfs[:4])
+    df_dev = df_train.sample(200)
     df_valid_test = dfs[4]
     df_valid, df_test = train_test_split(
         df_valid_test,
@@ -40,7 +35,6 @@ def load_spam_dataset():
 
     # TODO: Drop the label column for train
     return df_train, df_dev, df_valid, df_test
-
 
 if __name__ == "__main__":
     load_spam_dataset()
