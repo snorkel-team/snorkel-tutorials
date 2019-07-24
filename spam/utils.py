090b3a9dd0e8c5a99941e3db6bead5cb3df86499
Braden Hancock <hancock.braden@gmail.com>
2019-07-24 15:17:07 -0700

    Squash spam updates into single commit

--- spam/utils.py
+++ spam/utils.py
@@ -1,6 +1,7 @@
 import glob
 import subprocess
 
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -15,20 +16,26 @@ def load_spam_dataset():
     dfs = []
     for i, filename in enumerate(filenames, start=1):
         df = pd.read_csv(filename)
-        df["VIDEO_ID"] = [i] * len(df)
-        df = df.rename(columns={"CLASS": "LABEL"})
+        # Lowercase column names
+        df.columns = map(str.lower, df.columns)
+        # Remove comment_id field
+        df.drop("comment_id", axis=1)
+        # Add field indicating source video
+        df["video_id"] = [i] * len(df)
+        # Rename fields
+        df = df.rename(columns={"class": "label", "content": "text"})
         # Shuffle order
         df = df.sample(frac=1, random_state=123).reset_index(drop=True)
         dfs.append(df)
 
     df_train = pd.concat(dfs[:4])
     df_dev = df_train.sample(100, random_state=123)
+    df_train["label"] = np.ones(len(df_train["label"])) * -1
     df_valid_test = dfs[4]
     df_valid, df_test = train_test_split(
-        df_valid_test, test_size=0.5, random_state=123, stratify=df_valid_test.LABEL
+        df_valid_test, test_size=250, random_state=123, stratify=df_valid_test.label
     )
 
-    # TODO: Drop the label column for train
     return df_train, df_dev, df_valid, df_test
 
 
