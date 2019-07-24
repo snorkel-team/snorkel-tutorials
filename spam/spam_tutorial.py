eb18c11d123bc59799615a1e6c26c68e0010d4b1
Braden Hancock <hancock.braden@gmail.com>
2019-07-24 10:47:44 -0700

    Update section 2

--- spam/spam_tutorial.py
+++ spam/spam_tutorial.py
@@ -118,21 +118,29 @@ for split_name, df in [
 # ## 2. Writing Labeling Functions (LFs)
 
 # %% [markdown]
-# **Labeling functions (LFs) help users encode domain knowledge and other supervision sources programmatically**
+# **Labeling functions (LFs) help users encode domain knowledge and other supervision sources programmatically.**
 #
+# LFs are heuristics that take as input a data point and either assign a label to it (in this case, `HAM` or `SPAM`) or abstain (don't assign any label). Labeling functions can be *noisy*: they don't have perfect accuracy and don't have to label every data point.
 #
-# LFs are heuristics that take as input a datapoint and either assign a label to it (in this case, `HAM` or `SPAM`) or abstain (don't assign any label). Labeling functions can be *noisy*: they don't have perfect accuracy and don't g=have to label every datapoint.
-#
-# They can wrap several forms of supervision since the only requirement is that it is a function that takes as input a datapoint and outputs a label (or abstains) using any some deterministic logic. Example include, but are not limited to:
+# Because their only requirement is that they map a data point a label (or abstain), they can wrap a wide variety of forms of supervision. Examples include, but are not limited to:
 # * Keyword searches: looking for specific words in a sentence
 # * Pattern matching: looking for specific syntactical patterns
 # * Third-party models: using an pre-trained model (usually a model for a different task than the one at hand)
 # * Distant supervision: using external knowledge base
 # * Crowdworker labels: treating each crowdworker as a black-box function that assigns labels to subsets of the data
 #
-# The process of **developing LFs** is iterative and usually consists of encoding domain knowledge programmatically, measuring its performance on the dev set, editing it accordingly, and repeating for different LFs. After writing a few LFs, users look at the coverage of their LFs overall, and repeat the process to try to write LFs that cover subsets of the data that have not received labels from any LFs. **It is not necessary for LFs to assign labels to every datpoint; in fact, this is often the case.**
+# The process of **developing LFs** is iterative and usually consists of:
+# * Writing a function
+# * Estimating its performance by looking at labeled examples in the training set or dev set
+# * Iterating to improve coverage or accuracy as necessary. 
+# Balancing accuracy and coverage for specific labeling functions as well as the overall set of LFs developed is often a trade-off, and depending on the use case, users may tend to prefer one over the other. 
 #
-# Balancing accuracy and coverage for specific labeling functions as well as the overall set of LFs developed is often a trade-off, and depending on the use case, users may tend to prefer one over the other. Remember that the classifier that trains on labels from Snorkel has the power to _generalize_ and can therefore learn a good representation of the data even if the each datapoint in the training set does not receive a label from any LFs.
+# Once multiple LFs have been created, users can look at data points that have received no labels so far (or many conflicting labels) to get ideas for new LFs to write.
+# Another thing to check is whether there are any classes for which there are very few LFs, so that additional LFs can be written to cover that part of the dataset.
+# **Note, however, that it is not necessary for LFs to assign labels to every data point;** in fact, most of the time your LFs will not have perfect dataset-wide coverage.
+# We rely on the fact that the classifier that trains on labels from Snorkel has the power to _generalize_ and can therefore learn a good representation of the data even if the each data point in the training set does not receive a label from any LFs.
+#
+#
 
 # %% [markdown]
 # ### a) Look at examples for ideas
@@ -143,11 +151,6 @@ for split_name, df in [
 # %%
 # Display just the text and label
 df_dev[["text", "label"]].sample(10, random_state=123)
-
-# %%
-# for i, x in df_dev.iterrows():
-#     if "please" in x.text:
-#         print(x.text)
 
 # %% [markdown]
 # ### b) Write an LF
