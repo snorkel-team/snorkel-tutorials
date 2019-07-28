201833aa23ddcc2ef0162e72d51739ddb9fb2660
Alex Ratner <ajratner@gmail.com>
2019-07-27 20:33:48 -0700

    First rev on lighter-weight intro tutorial

---
+++ intro/download_data.sh
@@ -0,0 +1,15 @@
+# Execute from snorkel-tutorials/spam/
+# Download data,
+
+DATA_URL="https://archive.ics.uci.edu/ml/machine-learning-databases/00380/YouTube-Spam-Collection-v1.zip"
+
+if [ ! -d "data" ]; then
+    mkdir -p data
+    wget $DATA_URL -O data.zip
+    mv data.zip data/
+    cd data
+    unzip data.zip
+    rm data.zip
+    rm -rf __MACOSX
+    cd ..
+fi
