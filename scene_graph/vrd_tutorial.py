87e82df4b4141517a5a3d538823d38341c3d4115
Manas Joglekar <brahmaneya@gmail.com>
2019-07-29 17:44:04 +0000

    Remove percent file header

--- scene_graph/vrd_tutorial.py
+++ scene_graph/vrd_tutorial.py
@@ -1,19 +1,3 @@
-# -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:percent
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.2'
-#       jupytext_version: 1.2.0
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
 # %% [markdown]
 # # Visual Relationship Detection
 #
