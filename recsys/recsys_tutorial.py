1e91a7dc58cbd3b579163b1fbe7cc1c34a0ac1d6
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-08-08 18:54:28 +0000

    Recsys modeling work

--- recsys/recsys_tutorial.py
+++ recsys/recsys_tutorial.py
@@ -36,16 +36,28 @@ if os.path.basename(os.getcwd()) == "snorkel-tutorials":
 # TODO: Move downloading and processing code to data.py
 import gdown
 
-if not os.path.exists('data'):
-    os.mkdir('data')
-    os.chdir('data')
+if not os.path.exists("data"):
+    os.makedirs("data", exist_ok=True)
+    os.chdir("data")
     # Books
-    gdown.download('https://drive.google.com/uc?id=1H6xUV48D5sa2uSF_BusW-IBJ7PCQZTS1', output=None, quiet=None)
+    gdown.download(
+        "https://drive.google.com/uc?id=1gH7dG4yQzZykTpbHYsrw2nFknjUm0Mol",
+        output=None,
+        quiet=None,
+    )
     # Interactions
-    gdown.download('https://drive.google.com/uc?id=17G5_MeSWuhYnD4fGJMvKRSOlBqCCimxJ', output=None, quiet=None)
+    gdown.download(
+        "https://drive.google.com/uc?id=1NNX7SWcKahezLFNyiW88QFPAqOAYP5qg",
+        output=None,
+        quiet=None,
+    )
     # Reviews
-    gdown.download('https://drive.google.com/uc?id=1FVD3LxJXRc5GrKm97LehLgVGbRfF9TyO', output=None, quiet=None)
-    os.chdir('..')
+    gdown.download(
+        "https://drive.google.com/uc?id=1M5iqCZ8a7rZRtsmY5KQ5rYnP9S0bQJVo",
+        output=None,
+        quiet=None,
+    )
+    os.chdir("..")
 
 # %%
 import calendar
@@ -56,75 +68,190 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 
-month_to_int = dict((v,k) for k,v in enumerate(calendar.month_abbr))
+# %%
+month_to_int = dict((v, k) for k, v in enumerate(calendar.month_abbr))
+
 
 def get_timestamp(date_str):
     _, month, day, _, _, year = date_str.split()
     dt = datetime(year=int(year), month=month_to_int[month], day=int(day))
     return datetime.timestamp(dt)
 
-def load_data(file_name, max_to_load = 100):
+
+def load_data(file_name, max_to_load=100, filter_dict=None):
     count = 0
     data = []
+    filter_dict = filter_dict or {}
     with gzip.open(file_name) as fin:
         for l in fin:
             d = json.loads(l)
-            count += 1
-            data.append(d)
-            if (max_to_load is not None) and (count >= max_to_load):
-                break
+            for k, v in filter_dict.items():
+                if d[k] not in v:
+                    break
+            else:
+                count += 1
+                data.append(d)
+                if (max_to_load is not None) and (count >= max_to_load):
+                    break
     return data
 
-#book_suffix = '_poetry'
-book_suffix = '_comics_graphic'
+
+# %%
+# book_suffix = '_poetry'
+book_suffix = "_young_adult"
 
 books = load_data(f"data/goodreads_books{book_suffix}.json.gz", None)
 df_books = pd.DataFrame(books)
-# Make book_id column contiguous from 0 to len(df_books) - 1.
-book_to_idx = {bid: i for i, bid in enumerate(df_books.book_id.unique())}
-df_books.book_id = df_books.book_id.map(book_to_idx)
+
+df_books = df_books[
+    [
+        "authors",
+        "average_rating",
+        "book_id",
+        "country_code",
+        "description",
+        "is_ebook",
+        "language_code",
+        "popular_shelves",
+        "publication_year",
+        "ratings_count",
+        "series",
+        "similar_books",
+        "text_reviews_count",
+        "title",
+        "title_without_series",
+        "work_id",
+    ]
+]
+
+df_books = df_books.astype(
+    dict(
+        average_rating=float,
+        book_id=int,
+        is_ebook=bool,
+        ratings_count=int,
+        text_reviews_count=int,
+        work_id=int,
+    )
+)
+
 # Turns author role dict into list of <= 5 authors for simplicity.
-df_books.authors = df_books.authors.map(lambda l: [pair['author_id'] for pair in l[:5]])
-df_books['first_author'] = df_books.authors.map(lambda l: l[0])
-n_books = len(book_to_idx)
+df_books.authors = df_books.authors.map(lambda l: [pair["author_id"] for pair in l[:5]])
+df_books["first_author"] = df_books.authors.map(lambda l: int(l[0]))
 
-interactions = load_data(f"data/goodreads_interactions{book_suffix}.json.gz", None)
+# %%
+# %matplotlib inline
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+ratings = df_books.ratings_count.values
+
+print(np.percentile(ratings, [10, 50, 90, 95, 99]).astype(int))
+
+plt.hist(ratings.clip(0, 200), bins=100)
+plt.show()
+
+# %%
+min_ratings = 100
+max_ratings = 15000
+
+df_books_filt = df_books[
+    (df_books.ratings_count >= min_ratings) & (df_books.ratings_count <= max_ratings)
+]
+
+book_id_to_idx = {v: i for i, v in enumerate(df_books_filt.book_id)}
+df_books_filt["book_idx"] = df_books_filt.book_id.map(lambda bid: book_id_to_idx[bid])
+
+print(len(df_books), "books ->", len(df_books_filt), "books")
+df_books_filt.head()
+
+# %%
+filt_book_ids = set(map(str, book_id_to_idx.keys()))
+interactions = load_data(f"data/goodreads_interactions{book_suffix}.json.gz", None, dict(book_id=filt_book_ids))
 df_interactions = pd.DataFrame(interactions)
-df_interactions.book_id = df_interactions.book_id.map(book_to_idx)
-# Turn timestamp string into unix timestamp.
-df_interactions['timestamp'] = df_interactions.date_updated.map(get_timestamp)
-# Make user_id column continguous from 0 to num_users - 1.
-user_to_idx = {uid: i for i, uid in enumerate(df_interactions.user_id.unique())}
-df_interactions.user_id = df_interactions.user_id.map(user_to_idx)
-n_users = len(user_to_idx)
-# Map ratings to binary
+
+df_interactions = df_interactions[
+    ["book_id", "is_read", "rating", "review_id", "user_id"]
+]
+
+df_interactions = df_interactions.astype(dict(book_id=int, is_read=bool, rating=int))
+
+df_interactions["book_idx"] = df_interactions.book_id.map(
+    lambda bid: book_id_to_idx[bid]
+)
+
+df_interactions.head()
+
+# %%
+user_counts = df_interactions.groupby(["user_id"]).size().values
+
+print(np.percentile(user_counts, [10, 50, 75, 90, 95, 99]).astype(int))
+
+plt.hist(user_counts.clip(0, 200), bins=100)
+plt.show()
+
+# %%
+user_counts = df_interactions.groupby(["user_id"]).size()
+users_filt = user_counts[(user_counts >= 25) & (user_counts <= 200)].index
+user_id_to_idx = {v: i for i, v in enumerate(users_filt)}
+
+len(user_counts), len(user_id_to_idx)
+
+# %%
+df_interactions_filt = df_interactions[
+    df_interactions.user_id.isin(set(user_id_to_idx.keys()))
+]
+df_interactions_filt["user_idx"] = df_interactions_filt.user_id.map(
+    lambda uid: user_id_to_idx[uid]
+)
+
+print(len(df_interactions), len(df_interactions_filt))
+
+df_interactions_filt.head()
+
+# %%
+df_interactions_nz = df_interactions_filt[df_interactions_filt.rating != 0]
+df_interactions_z = df_interactions_filt[df_interactions_filt.rating == 0]
+
 ratings_map = {1: 0, 2: 0, 3: 0, 4: 1, 5: 1}
-df_interactions_nz = df_interactions[df_interactions.rating != 0]
-df_interactions_z = df_interactions[df_interactions.rating == 0]
-df_interactions.rating = df_interactions.rating.map(ratings_map)
-df_interactions_nz.rating = df_interactions_nz.rating.map(ratings_map)
-df_interactions_z.rating = -1
+df_interactions_nz["rating_4_5"] = df_interactions_nz.rating.map(
+    lambda r: ratings_map[r]
+)
 
-reviews = load_data(f"data/goodreads_reviews{book_suffix}.json.gz", None)
-df_reviews = pd.DataFrame(reviews)
-df_reviews['timestamp'] = df_reviews.date_updated.map(get_timestamp)
-df_reviews.book_id = df_reviews.book_id.map(book_to_idx)
-df_reviews.user_id = df_reviews.user_id.map(user_to_idx)
+ratings_map = {1: 0, 2: 0, 3: 0, 4: 0, 5: 1}
+df_interactions_nz["rating_5"] = df_interactions_nz.rating.map(lambda r: ratings_map[r])
 
-print(f"{len(user_to_idx)} users")
-print(f"{len(book_to_idx)} books")
-print(f"{len(interactions)} interactions")
-print(f"{len(reviews)} reviews")
+# %%
+# del df_books
+# del df_interactions
 
+print(f"{len(user_id_to_idx)} users")
+print(f"{len(book_id_to_idx)} books")
+print(f"{len(df_interactions_nz)} non-zero interactions")
+print(f"{len(df_interactions_z)} zero interactions")
+
+n_books = len(book_id_to_idx)
+
+# %%
 from sklearn.model_selection import train_test_split
 
 # Input data for FeedForward model
-user_to_books = (df_interactions.groupby('user_id')['book_id'].apply(tuple)
-                 .reset_index().rename(columns={'book_id': 'book_ids'}))
-data = user_to_books.merge(df_interactions_nz, on='user_id', how='inner')[['book_ids', 'book_id', 'rating']]
+user_to_books = (
+    df_interactions_filt.groupby("user_idx")["book_idx"]
+    .apply(tuple)
+    .reset_index()
+    .rename(columns={"book_idx": "book_idxs"})
+)
+data = user_to_books.merge(df_interactions_nz, on="user_idx", how="inner")[
+    ["book_idxs", "book_idx", "rating_4_5"]
+]
 data_train, data_test = train_test_split(data, test_size=0.1)
 data_train, data_dev = train_test_split(data_train, test_size=0.11111)
 data_train, data_val = train_test_split(data_train, test_size=0.125)
+
+# %%
+data_train.rating_4_5.mean()
 
 # %% [markdown]
 # ### FeedForward Model
@@ -138,51 +265,76 @@ early_stopping = tf.keras.callbacks.EarlyStopping(
     monitor="val_acc", patience=10, verbose=1, restore_best_weights=True
 )
 
+
 def recall_m(y_true, y_pred):
-        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-        recall = true_positives / (possible_positives + K.epsilon())
-        return recall
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
 
 def precision_m(y_true, y_pred):
-        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-        precision = true_positives / (predicted_positives + K.epsilon())
-        return precision
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
 
 def f1_m(y_true, y_pred):
     precision = precision_m(y_true, y_pred)
     recall = recall_m(y_true, y_pred)
-    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+    return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
+
 
 def get_feedforward_model():
-    num_b = tf.keras.layers.Input([], name='inp_num_b')
-    bids = tf.keras.layers.Input([None], name='inp_bids')
-    bid = tf.keras.layers.Input([], name='inp_bid')
-    bi_emb_dim = 128
+    num_b = tf.keras.layers.Input([], name="inp_num_b")
+    bids = tf.keras.layers.Input([None], name="inp_bids")
+    bid = tf.keras.layers.Input([], name="inp_bid")
+    bi_emb_dim = 64
     b_emb_dim = 64
-    layer_sizes = [40, 20, 10]
-    bi_emb = tf.keras.layers.Embedding(n_books, bi_emb_dim, name='bi_emb')(bids)
-    b_emb = tf.keras.layers.Embedding(n_books, b_emb_dim, name='b_emb')(bid)
-    bi_emb_reduced = tf.math.divide(tf.keras.backend.sum(bi_emb, axis=1), tf.expand_dims(num_b, 1), name='bi_emb_reduced')
-    input_layer = tf.keras.layers.concatenate([bi_emb_reduced, b_emb], 1, name='input_layer')
+    layer_sizes = [32]
+    bi_emb = tf.keras.layers.Embedding(n_books, bi_emb_dim, name="bi_emb")(bids)
+    b_emb = tf.keras.layers.Embedding(n_books, b_emb_dim, name="b_emb")(bid)
+    bi_emb_reduced = tf.math.divide(
+        tf.keras.backend.sum(bi_emb, axis=1),
+        tf.expand_dims(num_b, 1),
+        name="bi_emb_reduced",
+    )
+    input_layer = tf.keras.layers.concatenate(
+        [bi_emb_reduced, b_emb], 1, name="input_layer"
+    )
     cur_layer = input_layer
     for size in layer_sizes:
         tf.keras.layers.Dense(size, activation=tf.nn.relu)(cur_layer)
     output_layer = tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)(cur_layer)
-    feedforward_model = tf.keras.Model(inputs=[num_b, bids, bid], outputs=[output_layer])
-    feedforward_model.compile('Adagrad', 'binary_crossentropy', metrics=['accuracy', f1_m, precision_m, recall_m])
+    feedforward_model = tf.keras.Model(
+        inputs=[num_b, bids, bid], outputs=[output_layer]
+    )
+    feedforward_model.compile(
+        "Adagrad",
+        "binary_crossentropy",
+        metrics=["accuracy", f1_m, precision_m, recall_m],
+    )
     return feedforward_model
 
-padded_shapes = {'num_b': [], 'bids': [None], 'bid': [], 'label': []}
+
+padded_shapes = {"num_b": [], "bids": [None], "bid": [], "label": []}
+
 
 def get_data_tensors(data):
     def generator():
-        for bids, bid, rating in zip(data.book_ids, data.book_id, data.rating):
+        for bids, bid, rating in zip(data.book_idxs, data.book_idx, data.rating_4_5):
             if len(bids) <= 1:
                 continue
-            yield {'num_b': len(bids), 'bids': bids, 'bid': bid, 'label': rating}
-            yield {'num_b': len(bids), 'bids': bids, 'bid': np.random.randint(0, n_books), 'label': 0}
+            yield {"num_b": len(bids), "bids": bids, "bid": bid, "label": rating}
+            if rating == 1:
+                yield {
+                    "num_b": len(bids),
+                    "bids": bids,
+                    "bid": np.random.randint(0, n_books),
+                    "label": 0,
+                }
+
     dataset = (
         tf.data.Dataset.from_generator(generator, {k: tf.int64 for k in padded_shapes})
         .shuffle(123)
@@ -190,27 +342,34 @@ def get_data_tensors(data):
         .padded_batch(256, padded_shapes, drop_remainder=False)
     )
     tensor_dict = tf.compat.v1.data.make_one_shot_iterator(dataset).get_next()
-    return (tensor_dict["num_b"], tensor_dict["bids"], tensor_dict["bid"], tensor_dict["label"])
-
+    return (
+        tensor_dict["num_b"],
+        tensor_dict["bids"],
+        tensor_dict["bid"],
+        tensor_dict["label"],
+    )
 
 
 # %%
-#feedforward_model = get_feedforward_model()
+feedforward_model = get_feedforward_model()
 
 train_data_tensors = get_data_tensors(data_train)
 val_data_tensors = get_data_tensors(data_val)
 feedforward_model.fit(
-    train_data_tensors[:-1], train_data_tensors[-1], steps_per_epoch=300,
-    validation_data=(val_data_tensors[:-1], val_data_tensors[-1]), validation_steps=30,
-    callbacks=[early_stopping], epochs=20, verbose=1)
-
-test_data_tensors = get_data_tensors(data_test)
-feedforward_model.evaluate(test_data_tensors[:-1], test_data_tensors[-1], steps=30)
+    train_data_tensors[:-1],
+    train_data_tensors[-1],
+    steps_per_epoch=300,
+    validation_data=(val_data_tensors[:-1], val_data_tensors[-1]),
+    validation_steps=30,
+    # callbacks=[early_stopping],
+    epochs=100,
+    verbose=1,
+)
 
 # %%
 feedforward_model.evaluate(test_data_tensors[:-1], test_data_tensors[-1], steps=30)
-#probs = feedforward_model.predict(test_data_tensors[:-1], steps=30)
-#probs.reshape([256, 30])
+# probs = feedforward_model.predict(test_data_tensors[:-1], steps=30)
+# probs.reshape([256, 30])
 
 # %% [markdown]
 # A lot of books are part of a series (e.g. Naruto, The Sandman). The book titles are often of the form "<book_name> (series_name #number)". To capture the series, we use a regex to find non-numeric strings in between a '(' and '#'.
@@ -222,16 +381,23 @@ import re
 
 series_to_books = collections.defaultdict(set)
 for book_id, title in zip(df_books.book_id, df_books.title):
-    match = re.match('.*\(([^0-9]*)#', title)
+    match = re.match(".*\(([^0-9]*)#", title)
     if match:
         series_to_books[match.group(1)].add(book_id)
 # Only keep 'series' with at least 10 entries, as they are more likely to be an actual book series.
-series_to_books = {k:v for k, v in series_to_books.items() if len(v) > 10}
+series_to_books = {k: v for k, v in series_to_books.items() if len(v) > 10}
 print(list(series_to_books.keys())[:5])
 # Create reverse lookup dictionary.
-book_to_series = {book_id: series for series, book_set in series_to_books.items() for book_id in book_set}
+book_to_series = {
+    book_id: series
+    for series, book_set in series_to_books.items()
+    for book_id in book_set
+}
 
-@labeling_function(resources={"book_to_series": book_to_series, 'series_to_books': series_to_books})
+
+@labeling_function(
+    resources={"book_to_series": book_to_series, "series_to_books": series_to_books}
+)
 def many_same_series(x, book_to_series, series_to_books):
     # Abstain if book is not part of a series.
     if x.book_id not in book_to_series:
@@ -241,12 +407,24 @@ def many_same_series(x, book_to_series, series_to_books):
     return 1 if num_read > 1 else -1
 
 
-
 # %%
 book_to_first_author = dict(zip(df_books.book_id, df_books.first_author))
-user_author_ratings = data_train.merge(df_books[['book_id', 'first_author']], on='book_id')
-user_author_mean_ratings = user_author_ratings[['user_id', 'first_author', 'rating']].groupby(['user_id', 'first_author']).agg('mean').reset_index()
-user_author_ratings_dict = dict(zip(zip(user_author_mean_ratings.user_id, user_author_mean_ratings.first_author), user_author_mean_ratings.rating))
+user_author_ratings = data_train.merge(
+    df_books[["book_id", "first_author"]], on="book_id"
+)
+user_author_mean_ratings = (
+    user_author_ratings[["user_id", "first_author", "rating"]]
+    .groupby(["user_id", "first_author"])
+    .agg("mean")
+    .reset_index()
+)
+user_author_ratings_dict = dict(
+    zip(
+        zip(user_author_mean_ratings.user_id, user_author_mean_ratings.first_author),
+        user_author_mean_ratings.rating,
+    )
+)
+
 
 @labeling_function()
 def common_first_author(x):
@@ -261,8 +439,8 @@ from snorkel.labeling.apply import PandasLFApplier
 from snorkel.labeling.analysis import LFAnalysis
 
 # same_first_author is slower but more accuarate than common_author (0.68)
-# The difference is we take mean and np.around in same_author but pick random entry in common_author. 
-lfs = [many_same_series]#, common_first_author]# + shelf_lfs
+# The difference is we take mean and np.around in same_author but pick random entry in common_author.
+lfs = [many_same_series]  # , common_first_author]# + shelf_lfs
 
 applier = PandasLFApplier(lfs)
 L_dev = applier.apply(data_dev)
