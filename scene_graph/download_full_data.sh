c42e36a8c79aaf5526ad4e481509a1f6517dc2ad
Paroma Varma <varma.paroma@gmail.com>
2019-08-07 17:41:53 -0700

    separate download scripts

---
+++ scene_graph/download_full_data.sh
@@ -0,0 +1,37 @@
+# Execute from snorkel-tutorials/
+# Download data,
+
+ANNOTATIONS_URL="https://www.dropbox.com/s/bnfhm6kt9xumik8/vrd.zip"
+IMAGES_URL="http://imagenet.stanford.edu/internal/jcjohns/scene_graphs/sg_dataset.zip"
+SAMPLE_IMAGES_URL="https://github.com/Prof-Lu-Cewu/Visual-Relationship-Detection.git"
+GLOVE_URL="http://nlp.stanford.edu/data/wordvecs/glove.6B.zip"
+
+if [ ! -d "scene_graph/data" ]; then
+    mkdir -p scene_graph/data
+    cd scene_graph/data
+
+    # download and unzip metadata and annotations
+    wget $ANNOTATIONS_URL
+    unzip vrd.zip
+
+    # Delete the zip files.
+    rm vrd.zip
+    cd VRD
+
+    # Download and unzip all images
+    wget $IMAGES_URL
+    unzip sg_dataset.zip
+    rm sg_dataset.zip
+    cd ../../..
+
+    mkdir -p scene_graph/data/glove
+    cd scene_graph/data/glove
+
+    wget $GLOVE_URL
+    unzip glove.6B.zip
+
+    # Delete the zip files
+    rm  glove.6B.zip
+    cd ../../..
+fi
+
--- scene_graph/vrd_tutorial.py
+++ scene_graph/vrd_tutorial.py
@@ -1,4 +1,19 @@
-# %% [markdown]
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
 # # Visual Relationship Detection
 #
 # In this tutorial, we focus on the task of classifying visual relationships. For a given image, there might be many such relationships, defined formally as a `subject <predictate> object` (e.g. `person <riding> bike`).
@@ -11,13 +26,13 @@
 #
 # In the examples of the relationships shown below, the red box represents the _subject_ while the green box represents the _object_. The _predicate_ (e.g. kick) denotes what relationship connects the subject and the object.
 
-# %%
+# +
 import os
 
 if os.path.basename(os.getcwd()) == "scene_graph":
     os.chdir("..")
+# -
 
-# %% [markdown]
 # ### 1. Load Dataset
 # We load the VRD dataset and filter images with at least one action predicate in it, since these are more difficult to classify than geometric relationships like `above` or `next to`. We load the train, valid, and test sets as Pandas DataFrame objects with the following fields:
 # - `label`: The relationship between the objects. 0: `RIDE`, 1: `CARRY`, 2: `OTHER` action predicates
@@ -26,38 +41,44 @@ if os.path.basename(os.getcwd()) == "scene_graph":
 # - `source_img`: filename for the corresponding image the relationship is in
 # - `subject_bbox`: coordinates of the bounding box for the object `[ymin, ymax, xmin, xmax]`
 # - `subject_category`: category of the subject
-#
-# Note that the training DataFrame will have a labels field with all -1s. This denotes the lack of labels for that particular dataset. In this tutorial, we will assign probabilistic labels to the training set by writing labeling functions over attributes of the subject and objects!
 
-# %%
+# +
 # %load_ext autoreload
 # %autoreload 2
 
 import numpy as np
+# -
 
-# %%
+# If you are running this notebook for the first time, it will take ~15 mins to download all the required data. **Setting `sample = False` will download the full VRD dataset, which can take ~3 hours!**
+#
+# The sampled version of the dataset **uses the same 26 examples across the train, dev, and test sets. This setting is meant to demonstrate how Snorkel works with this task, NOT TO DEMONSTRATE PERFORMANCE.**
+#
+
+# +
 from scene_graph.utils import load_vrd_data
 
-train_df, valid_df, test_df = load_vrd_data()
+sample = True
+is_travis = 'TRAVIS' in os.environ
+train_df, valid_df, test_df = load_vrd_data(sample, is_travis)
 
 print("Train Relationships: ", len(train_df))
 print("Dev Relationships: ", len(valid_df))
 print("Test Relationships: ", len(test_df))
+# -
 
-# %% [markdown]
+# Note that the training DataFrame will have a labels field with all -1s. This denotes the lack of labels for that particular dataset. In this tutorial, we will assign probabilistic labels to the training set by writing labeling functions over attributes of the subject and objects!
+
 # ## 2. Writing Labeling Functions
 # We now write labeling functions to detect what relationship exists between pairs of bounding boxes. To do so, we can encode various intuitions into the labeling functions. _Categorical_ intution: knowledge about the categories of subjects and objects usually involved in these relationships (e.g., `person` is usually the subject for predicates like `ride` and `carry`), and _spatial_ intuition: knowledge about the relative positions of the subject and objects (e.g., subject is usually higher than the object for the predicate `ride`).
 
-# %%
 RIDE = 0
 CARRY = 1
 OTHER = 2
 ABSTAIN = -1
 
-# %% [markdown]
 # We begin with labeling functions that encode categorical intuition: we use knowledge about common subject-object category pairs that are common for `RIDE` and `CARRY` and also knowledge about what subjects or objects are unlikely to be involved in the two relationships.
 
-# %%
+# +
 from snorkel.labeling import labeling_function
 
 # Category-based LFs
@@ -100,10 +121,11 @@ def LF_person(x):
     return ABSTAIN
 
 
-# %% [markdown]
+# -
+
 # We now encode our spatial intuition, which includes measuring the distance between the bounding boxes and comparing their relative areas.
 
-# %%
+# +
 # Distance-based LFs
 @labeling_function()
 def LF_ydist(x):
@@ -134,10 +156,11 @@ def LF_area(x):
     return ABSTAIN
 
 
-# %% [markdown]
+# -
+
 # Note that the labeling functions have varying empirical accuracies and coverages. Due to class imbalance in our chosen relationships, labeling functions that label the `OTHER` class have higher coverage than labeling functions for `RIDE` or `CARRY`. This reflects the distribution of classes in the dataset as well.
 
-# %%
+# +
 from snorkel.labeling import PandasLFApplier
 
 lfs = [
@@ -155,36 +178,33 @@ applier = PandasLFApplier(lfs)
 L_train = applier.apply(train_df)
 L_valid = applier.apply(valid_df)
 
-# %%
+# +
 from snorkel.labeling import LFAnalysis
 
 Y_valid = valid_df.label.values
 LFAnalysis(L_valid, lfs).lf_summary(Y_valid)
+# -
 
-# %% [markdown]
 # ## 3. Train Label Model
 # We now train a multi-class `LabelModel` to assign training labels to the unalabeled training set.
 
-# %%
+# +
 from snorkel.labeling import LabelModel
 
 label_model = LabelModel(cardinality=3, verbose=True)
 label_model.fit(L_train, seed=123, lr=0.01, log_freq=10, n_epochs=100)
+# -
 
-# %% [markdown]
 # We use [F1](https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html) Micro average for the multiclass setting, which calculates metrics globally by counting the total true positives, false negatives and false positives.
 
-# %%
 label_model.score(L_valid, Y_valid, metrics=["f1_micro"])
 
-# %% [markdown]
 # ## 4. Train a Classifier
 # You can then use these training labels to train any standard discriminative model, such as [an off-the-shelf ResNet](https://github.com/KaimingHe/deep-residual-networks), which should learn to generalize beyond the LF's we've developed!
 
-# %% [markdown]
 # #### Create DataLoaders for Classifier
 
-# %%
+# +
 from snorkel.classification import DictDataLoader
 from scene_graph.model import FlatConcat, SceneGraphDataset, WordEmb, init_fc
 
@@ -203,11 +223,11 @@ valid_dl = DictDataLoader(
     batch_size=16,
     shuffle=False,
 )
+# -
 
-# %% [markdown]
 # #### Define Model Architecture
 
-# %%
+# +
 import torchvision.models as models
 import torch.nn as nn
 
@@ -241,7 +261,7 @@ module_pool = nn.ModuleDict(
     }
 )
 
-# %%
+# +
 from scene_graph.model import get_task_flow
 
 # define task flow through modules
@@ -252,8 +272,8 @@ pred_cls_task = Task(
     task_flow=task_flow,
     scorer=Scorer(metrics=["f1_micro"]),
 )
+# -
 
-# %% [markdown]
 # ### Train and Evaluate Model
 
 # %%
@@ -267,6 +287,6 @@ trainer = Trainer(
     checkpointer_config={"checkpoint_dir": "checkpoint"},
 )
 trainer.fit(model, [train_dl])
+# -
 
-# %%
 model.score([valid_dl])
