ccd9d13df011dddf26d94d50447be3c459e22902
Manas Joglekar <brahmaneya@gmail.com>
2019-08-08 05:56:01 +0000

    fix typo

--- crowdsourcing/download-data.sh
+++ crowdsourcing/download-data.sh
@@ -4,6 +4,6 @@ trap 'rm -r data/' ERR  # Clean up in case of error.
 
 if [ ! -d "data" ]; then
     mkdir -p data
-    wget https://abcdraw.githubusercontent.com/HazyResearch/snorkel/master/tutorials/crowdsourcing/data/weather-non-agg-DFE.csv -P data
+    wget https://raw.githubusercontent.com/HazyResearch/snorkel/master/tutorials/crowdsourcing/data/weather-non-agg-DFE.csv -P data
     wget https://d1p17r2m4rzlbo.cloudfront.net/wp-content/uploads/2016/03/weather-evaluated-agg-DFE.csv -P data
 fi
