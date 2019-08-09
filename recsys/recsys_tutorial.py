64dc81ce57e1c6146a7623f96245bfcdcdcc9d46
Manas Joglekar <brahmaneya@gmail.com>
2019-08-09 00:01:25 +0000

    Add review processing and LFs.

--- recsys/recsys_tutorial.py
+++ recsys/recsys_tutorial.py
@@ -168,7 +168,7 @@ df_books_filt.head()
 
 # %%
 filt_book_ids = set(map(str, book_id_to_idx.keys()))
-interactions = load_data(f"data/goodreads_interactions{book_suffix}.json.gz", None, dict(book_id=filt_book_ids))
+interactions = load_data(f"data/goodreads_interactions{book_suffix}.json.gz", 5000000, dict(book_id=filt_book_ids))
 df_interactions = pd.DataFrame(interactions)
 
 df_interactions = df_interactions[
@@ -223,6 +223,12 @@ ratings_map = {1: 0, 2: 0, 3: 0, 4: 0, 5: 1}
 df_interactions_nz["rating_5"] = df_interactions_nz.rating.map(lambda r: ratings_map[r])
 
 # %%
+reviews = load_data(f"data/goodreads_reviews{book_suffix}.json.gz", None, dict(book_id=filt_book_ids, user_id=set(user_id_to_idx.keys())))
+df_reviews = pd.DataFrame(reviews)
+df_reviews["book_idx"] = df_reviews.book_id.astype('int').map(book_id_to_idx)
+df_reviews["user_idx"] = df_reviews.user_id.map(user_id_to_idx)
+
+# %%
 # del df_books
 # del df_interactions
 
@@ -230,6 +236,7 @@ print(f"{len(user_id_to_idx)} users")
 print(f"{len(book_id_to_idx)} books")
 print(f"{len(df_interactions_nz)} non-zero interactions")
 print(f"{len(df_interactions_z)} zero interactions")
+print(f"{len(df_reviews)} reviews")
 
 n_books = len(book_id_to_idx)
 
@@ -244,11 +251,19 @@ user_to_books = (
     .rename(columns={"book_idx": "book_idxs"})
 )
 data = user_to_books.merge(df_interactions_nz, on="user_idx", how="inner")[
-    ["book_idxs", "book_idx", "rating_4_5"]
-]
-data_train, data_test = train_test_split(data, test_size=0.1)
-data_train, data_dev = train_test_split(data_train, test_size=0.11111)
-data_train, data_val = train_test_split(data_train, test_size=0.125)
+    ["user_idx", "book_idxs", "book_idx", "rating_4_5"]
+].merge(
+    df_reviews[['user_idx', 'book_idx', 'review_text']], on=['user_idx', 'book_idx'], how='left'
+)
+user_idxs = list(user_id_to_idx.values())
+user_idxs_train, user_idxs_test = train_test_split(user_idxs, test_size=0.1)
+user_idxs_train, user_idxs_dev = train_test_split(user_idxs_train, test_size=0.11111)
+user_idxs_train, user_idxs_val = train_test_split(user_idxs_train, test_size=0.125)
+
+data_train = data[data.user_idx.isin(set(user_idxs_train))]
+data_test = data[data.user_idx.isin(set(user_idxs_test))]
+data_dev = data[data.user_idx.isin(set(user_idxs_dev))]
+data_val = data[data.user_idx.isin(set(user_idxs_val))]
 
 # %%
 data_train.rating_4_5.mean()
@@ -256,6 +271,149 @@ data_train.rating_4_5.mean()
 # %% [markdown]
 # ### FeedForward Model
 # This model learns two embeddings per book. It takes an input a set of book ratings, and a new book, and tries to predict its rating. (So it models a user as a set of book-rating pairs, instead of with a separate embedding).
+
+# %% [markdown]
+# A lot of books are part of a series (e.g. Naruto, The Sandman). The book titles are often of the form "<book_name> (series_name #number)". To capture the series, we use a regex to find non-numeric strings in between a '(' and '#'.
+
+# %%
+# TODO: This only works for comics
+from snorkel.labeling.lf import labeling_function
+import re
+
+series_to_books = collections.defaultdict(set)
+for book_idx, title in zip(df_books_filt.book_idx, df_books_filt.title):
+    match = re.match(".*\(([^0-9]*)#", title)
+    if match:
+        series_to_books[match.group(1)].add(book_idx)
+# Only keep 'series' with at least 10 entries, as they are more likely to be an actual book series.
+series_to_books = {k: v for k, v in series_to_books.items() if len(v) > 10}
+print(list(series_to_books.keys())[:5])
+# Create reverse lookup dictionary.
+book_to_series = {
+    book_idx: series
+    for series, book_set in series_to_books.items()
+    for book_idx in book_set
+}
+
+
+@labeling_function(
+    resources={"book_to_series": book_to_series, "series_to_books": series_to_books}
+)
+def many_same_series(x, book_to_series, series_to_books):
+    # Abstain if book is not part of a series.
+    if x.book_idx not in book_to_series:
+        return -1
+    same_series_books = series_to_books[book_to_series[x.book_idx]]
+    num_read = len(set(x.book_idxs).intersection(same_series_books))
+    return 1 if num_read > 4 else -1
+
+
+# %%
+book_to_first_author = dict(zip(df_books_filt.book_idx, df_books_filt.first_author))
+author_to_bookd_df = df_books_filt.groupby('first_author')[['book_idx']].agg(set)
+author_to_books = dict(zip(author_to_bookd_df.index, author_to_bookd_df.book_idx))
+
+@labeling_function()
+def common_first_author(x):
+    author = book_to_first_author[x.book_idx]
+    same_author_books = author_to_books[author]
+    num_read = len(set(x.book_idxs).intersection(same_author_books))
+    return 1 if num_read > 6 else -1
+
+
+# %%
+from snorkel.preprocess import preprocessor
+from textblob import TextBlob
+
+@preprocessor()
+def textblob_polarity(x):
+    if x.review_text:
+        x.blob = TextBlob(str(x.review_text))
+        x.start_blob = TextBlob(' '.join(x.blob.raw_sentences[:2]))
+    else:
+        x.blob = None
+    return x
+
+textblob_polarity.memoize = True
+
+# Label high polarity reviews as positive.
+@labeling_function(pre=[textblob_polarity])
+def polarity_positive(x):
+    if x.blob:
+        if x.blob.polarity > 0.3:
+            return 1
+    return -1
+
+# Label reviews with high polarity starting sentences as positive.
+@labeling_function(pre=[textblob_polarity])
+def starting_polarity_positive(x):
+    if x.blob:
+        if x.start_blob.polarity > 0.5:
+            return 1
+    return -1
+
+# Label reviews with low polarity starting sentences as negative.
+@labeling_function(pre=[textblob_polarity])
+def starting_polarity_negative(x):
+    if x.blob:
+        if x.start_blob.polarity < -0.2:
+            return 0
+    return -1
+
+# Label high subjectivity reviews as positive.
+@labeling_function(pre=[textblob_polarity])
+def subjectivity_positive(x):
+    if x.blob:
+        if x.blob.subjectivity > 0.75:
+            return 1
+    return -1
+
+# Label low polarity reviews as negative.
+@labeling_function(pre=[textblob_polarity])
+def polarity_negative(x):
+    if x.blob:
+        if x.blob.polarity < 0.0:
+            return 0
+    return -1
+
+
+
+# %%
+from snorkel.labeling import PandasLFApplier, LFAnalysis
+
+lfs = [common_first_author, starting_polarity_positive, starting_polarity_negative, polarity_positive, subjectivity_positive, polarity_negative]
+
+applier = PandasLFApplier(lfs)
+L_dev = applier.apply(data_dev)
+LFAnalysis(L_dev, lfs).lf_summary(data_dev.rating_4_5)
+
+# %%
+from snorkel.labeling.model.label_model import LabelModel
+
+# Train LabelModel.
+L_train = applier.apply(data_train)
+label_model = LabelModel(cardinality=2, verbose=True)
+label_model.fit(L_train, n_epochs=5000, seed=123, log_freq=20, lr=0.01)
+
+from snorkel.analysis import metric_score
+from snorkel.utils import probs_to_preds
+
+Y_dev_prob = label_model.predict_proba(L_dev)
+Y_dev_pred = probs_to_preds(Y_dev_prob)
+
+acc = metric_score(data_dev.rating_4_5, Y_dev_pred, probs=None, metric="accuracy")
+print(f"LabelModel Accuracy: {acc:.3f}")
+
+Y_train_prob = label_model.predict_proba(L_train)
+Y_train_preds = probs_to_preds(Y_train_prob)
+
+# %%
+# Create new training examples using LF.
+from snorkel.labeling import filter_unlabeled_dataframe
+data_train_filtered, Y_train_prob_filtered = filter_unlabeled_dataframe(data_train, Y_train_prob, L_train)
+Y_train_preds_filtered = probs_to_preds(Y_train_prob_filtered)
+data_train_filtered['rating_4_5'] = Y_train_preds_filtered
+combined_data_train = pd.concat([data_train_filtered, data_dev], axis=0)
 
 # %%
 import tensorflow as tf
@@ -353,7 +511,7 @@ def get_data_tensors(data):
 # %%
 feedforward_model = get_feedforward_model()
 
-train_data_tensors = get_data_tensors(data_train)
+train_data_tensors = get_data_tensors(combined_data_train)
 val_data_tensors = get_data_tensors(data_val)
 feedforward_model.fit(
     train_data_tensors[:-1],
@@ -362,117 +520,10 @@ feedforward_model.fit(
     validation_data=(val_data_tensors[:-1], val_data_tensors[-1]),
     validation_steps=30,
     # callbacks=[early_stopping],
-    epochs=100,
+    epochs=40,
     verbose=1,
 )
 
 # %%
+test_data_tensors = get_data_tensors(data_test)
 feedforward_model.evaluate(test_data_tensors[:-1], test_data_tensors[-1], steps=30)
-# probs = feedforward_model.predict(test_data_tensors[:-1], steps=30)
-# probs.reshape([256, 30])
-
-# %% [markdown]
-# A lot of books are part of a series (e.g. Naruto, The Sandman). The book titles are often of the form "<book_name> (series_name #number)". To capture the series, we use a regex to find non-numeric strings in between a '(' and '#'.
-
-# %%
-# TODO: This only works for comics
-from snorkel.labeling.lf import labeling_function
-import re
-
-series_to_books = collections.defaultdict(set)
-for book_id, title in zip(df_books.book_id, df_books.title):
-    match = re.match(".*\(([^0-9]*)#", title)
-    if match:
-        series_to_books[match.group(1)].add(book_id)
-# Only keep 'series' with at least 10 entries, as they are more likely to be an actual book series.
-series_to_books = {k: v for k, v in series_to_books.items() if len(v) > 10}
-print(list(series_to_books.keys())[:5])
-# Create reverse lookup dictionary.
-book_to_series = {
-    book_id: series
-    for series, book_set in series_to_books.items()
-    for book_id in book_set
-}
-
-
-@labeling_function(
-    resources={"book_to_series": book_to_series, "series_to_books": series_to_books}
-)
-def many_same_series(x, book_to_series, series_to_books):
-    # Abstain if book is not part of a series.
-    if x.book_id not in book_to_series:
-        return -1
-    same_series_books = series_to_books[book_to_series[x.book_id]]
-    num_read = len(set(x.book_ids).intersection(same_series_books))
-    return 1 if num_read > 1 else -1
-
-
-# %%
-book_to_first_author = dict(zip(df_books.book_id, df_books.first_author))
-user_author_ratings = data_train.merge(
-    df_books[["book_id", "first_author"]], on="book_id"
-)
-user_author_mean_ratings = (
-    user_author_ratings[["user_id", "first_author", "rating"]]
-    .groupby(["user_id", "first_author"])
-    .agg("mean")
-    .reset_index()
-)
-user_author_ratings_dict = dict(
-    zip(
-        zip(user_author_mean_ratings.user_id, user_author_mean_ratings.first_author),
-        user_author_mean_ratings.rating,
-    )
-)
-
-
-@labeling_function()
-def common_first_author(x):
-    author = book_to_first_author[x.book_id]
-    if (x.user_id, author) in user_author_ratings_dict:
-        return int(np.around(user_author_ratings_dict[(x.user_id, author)]))
-    return -1
-
-
-# %%
-from snorkel.labeling.apply import PandasLFApplier
-from snorkel.labeling.analysis import LFAnalysis
-
-# same_first_author is slower but more accuarate than common_author (0.68)
-# The difference is we take mean and np.around in same_author but pick random entry in common_author.
-lfs = [many_same_series]  # , common_first_author]# + shelf_lfs
-
-applier = PandasLFApplier(lfs)
-L_dev = applier.apply(data_dev)
-LFAnalysis(L_dev, lfs).lf_summary(data_dev.rating)
-
-# %%
-{k: v / float(len(data_dev)) for k, v in Counter(data_dev.rating).items()}
-
-# %%
-from snorkel.labeling.model.label_model import LabelModel
-
-# Train LabelModel.
-L_train = applier.apply(df_interactions_z)
-label_model = LabelModel(cardinality=6, verbose=True)
-label_model.fit(L_train, n_epochs=5000, seed=123, log_freq=20, lr=0.01)
-
-from snorkel.analysis.metrics import metric_score
-from snorkel.analysis.utils import probs_to_preds
-
-Y_dev_prob = label_model.predict_proba(L_dev)
-Y_dev_pred = probs_to_preds(Y_dev_prob)
-
-acc = metric_score(data_dev.rating, Y_dev_pred, probs=None, metric="accuracy")
-print(f"LabelModel Accuracy: {acc:.3f}")
-
-Y_train_prob = label_model.predict_proba(L_train)
-Y_train_preds = probs_to_preds(Y_train_prob)
-
-# %%
-# Create new training examples using LF.
-L_train = applier.apply(df_interactions_z)
-new_idxs = np.where(L_train.max(1) != -1)[0]
-new_data_train = df_interactions_z.iloc[new_idxs]
-new_data_train.rating = L_train[new_idxs]
-combined_data_train = pd.concat([data_train, new_data_train], axis=0)
