fed198ca87a498bbe34a2a776c7adebe61cbe24f
Braden Hancock <hancock.braden@gmail.com>
2019-08-09 16:55:19 -0700

    Revisions to crowdsourcing tutorial

--- crowdsourcing/crowdsourcing_tutorial.py
+++ crowdsourcing/crowdsourcing_tutorial.py
@@ -1,73 +1,61 @@
 # %% [markdown]
-# # Crowdsourcing tutorial
-# In this tutorial, we'll provide a simple walkthrough of how to use Snorkel alongside crowdsourcing to generate labels for a sentiment analysis task.
-# We have crowdsourced labels for about half of the training dataset.
-# The crowdsourcing labels are of a fairly high quality, but do not cover the entire training dataset, nor are they available for the test set or during inference.
-# To make up for their lack of training set coverage, we combine crowdsourcing labels with heuristic labeling functions to increase the number of training labels we have.
+# # Crowdsourcing Tutorial
+
+# %% [markdown]
+# In this tutorial, we'll provide a simple walkthrough of how to use Snorkel in conjuction with crowdsourcing to create a training set for a sentiment analysis task.
+# We already have crowdsourced labels for about half of the training dataset.
+# The crowdsourced labels are fairly accurate, but do not cover the entire training dataset, nor are they available for the test set or during inference.
+# To make up for their lack of training set coverage, we combine crowdsourced labels with heuristic labeling functions to increase the number of training labels we have.
 # Like most Snorkel labeling pipelines, we'll use the denoised labels to train a deep learning
-# model which can be applied to new, unseen data to automatically make predictions!
+# model which can be applied to new, unseen data to automatically make predictions.
+
+# %% [markdown]
+# ## Dataset Details
+
+# %% [markdown]
+# In this tutorial, we'll use the [Weather Sentiment](https://data.world/crowdflower/weather-sentiment) dataset from Figure Eight.
+# Our goal is to train a classifier that can label new tweets as expressing either a positive or negative sentiment.
 #
-# In this tutorial, we're using the
-# [Weather Sentiment](https://data.world/crowdflower/weather-sentiment)
-# dataset from Figure Eight.
-# Our goal is to label each tweet as either positive or negative so that
-# we can train a language model over the tweets themselves that can be applied
-# to new, unseen data points.
-# Crowd workers were asked to grade the sentiment of a
-# particular tweet relating to the weather. They could say it was positive or
-# negative, or choose one of three other options saying they weren't sure it was
-# positive or negative.
-#
-# The catch is that 20 crowd workers graded each tweet, and in many cases
-# crowd workers assigned conflicting sentiment labels to the same tweet.
+# Crowdworkers were asked to label the sentiment of a particular tweet relating to the weather.
+# The catch is that 20 crowdworkers graded each tweet, and in many cases crowdworkers assigned conflicting sentiment labels to the same tweet.
 # This is a common issue when dealing with crowdsourced labeling workloads.
 #
-# We've also altered the data set to reflect a realistic crowdsourcing pipeline
-# where only a subset of our full training set have recieved crowd labels.
-# Since our objective is to classify tweets as positive or negative, we limited
-# the dataset to tweets that were either positive or negative.
+# Label options were positive, negative, or one of three other options saying they weren't sure if it was positive or negative; we use only the positive/negative labels.
+# We've also altered the dataset to reflect a realistic crowdsourcing pipeline where only a subset of our available training set have recieved crowd labels.
 #
-# We'll encode the crowd labels themselves as labeling functions in order
-# to learn trust weights for each crowd worker, and write a few heuristic
-# labeling functions to cover the data points without crowd labels.
-# Snorkel's ability to build high-quality datasets from multiple noisy labeling
-# signals makes it an ideal framework to approach this problem.
+# We will treat each crowdworker's labels as coming from a single labeling function (LF).
+# This will allow us to learn a weight for how much much to trust the labels from each crowdworker.
+# We will also write a few heuristic labeling functions to cover the data points without crowd labels.
+# Snorkel's ability to build high-quality datasets from multiple noisy labeling signals makes it an ideal framework to approach this problem.
+
+# %% [markdown]
+# ## Loading Crowdsourcing Dataset
 
 # %% [markdown]
 # We start by loading our data which has 287 examples in total.
 # We take 50 for our development set and 50 for our test set.
 # The remaining 187 examples form our training set.
-# This data set is very small, and we're primarily using it for demonstration purposes.
-# In particular, we'd expect to have access to many more unlabeled tweets in order to
-# train a high performance text model.
-# Since the dataset is already small, we skip
-# using a validation set.
-#
-# The labels above have been mapped to integers, which we show here.
-
-# %% [markdown]
-# ## Loading Crowdsourcing Dataset
+# Since the dataset is already small, we skip using a validation set.
+# Note that this very small dataset is primarily used for demonstration purposes here.
+# In a real setting, we would expect to have access to many more unlabeled tweets, which could help us to train a higher quality model.
 
 # %%
 import os
 
+# Make sure we're in the right directory
 if os.path.basename(os.getcwd()) == "snorkel-tutorials":
     os.chdir("crowdsourcing")
 
 # %%
-from data import load_data, answer_mapping
+from data import load_data
 
-crowd_answers, df_train, df_dev, df_test = load_data()
+crowd_labels, df_train, df_dev, df_test = load_data()
 Y_dev = df_dev.sentiment.values
 Y_test = df_test.sentiment.values
 
-print("Answer to int mapping:")
-for k, v in sorted(answer_mapping.items(), key=lambda kv: kv[1]):
-    print(f"{k:<50}{v}")
-
 # %% [markdown]
-# First, let's take a look at our development set to get a sense of
-# what the tweets look like.
+# First, let's take a look at our development set to get a sense of what the tweets look like.  
+# We use the following label convention: 0 = Negative, 1 = Positive.
 
 # %%
 import pandas as pd
@@ -82,56 +70,44 @@ df_dev.head()
 # We'll convert these into labeling functions.
 
 # %%
-crowd_answers.head()
+crowd_labels.head()
 
 # %% [markdown]
 # ## Writing Labeling Functions
-# Each crowd worker can be thought of as a single labeling function,
+# Each crowdworker can be thought of as a single labeling function,
 # as each worker labels a subset of examples,
-# and may have errors or conflicting answers with other workers / labeling functions.
+# and may have errors or conflicting labels with other workers / labeling functions.
 # So we create one labeling function per worker.
 # We'll simply return the label the worker submitted for a given tweet, and abstain
-# if they didn't submit an answer for it.
+# if they didn't submit a label for it.
 
 # %% [markdown]
-# ### Crowd worker labeling functions
+# ### Crowdworker labeling functions
 
 # %%
-labels_by_annotator = crowd_answers.groupby("worker_id")
+labels_by_annotator = crowd_labels.groupby("worker_id")
 worker_dicts = {}
 for worker_id in labels_by_annotator.groups:
-    worker_df = labels_by_annotator.get_group(worker_id)[["answer"]]
-    if len(worker_df) > 10:
-        worker_dicts[worker_id] = dict(zip(worker_df.index, worker_df.answer))
+    worker_df = labels_by_annotator.get_group(worker_id)[["label"]]
+    worker_dicts[worker_id] = dict(zip(worker_df.index, worker_df.label))
 
 print("Number of workers:", len(worker_dicts))
 
 # %%
 from snorkel.labeling import LabelingFunction
 
+ABSTAIN = -1
 
-def f_pos(x, worker_dict):
-    label = worker_dict.get(x.tweet_id)
-    return 1 if label == 1 else -1
+def worker_lf(x, worker_dict):
+    return worker_dict.get(x.tweet_id, ABSTAIN)
 
-
-def f_neg(x, worker_dict):
-    label = worker_dict.get(x.tweet_id)
-    return 0 if label == 0 else -1
-
-
-def get_worker_labeling_function(worker_id, f):
+def make_worker_lf(worker_id):
     worker_dict = worker_dicts[worker_id]
-    name = f"worker_{worker_id}_{f.__name__}"
-    return LabelingFunction(name, f=f, resources={"worker_dict": worker_dict})
+    name = f"worker_{worker_id}"
+    return LabelingFunction(name, f=worker_lf, resources={"worker_dict": worker_dict})
 
 
-worker_lfs_pos = [
-    get_worker_labeling_function(worker_id, f_pos) for worker_id in worker_dicts
-]
-worker_lfs_neg = [
-    get_worker_labeling_function(worker_id, f_neg) for worker_id in worker_dicts
-]
+worker_lfs = [make_worker_lf(worker_id) for worker_id in worker_dicts]
 
 # %% [markdown]
 # Let's take a quick look at how well they do on the development set.
@@ -139,29 +115,31 @@ worker_lfs_neg = [
 # %%
 from snorkel.labeling import PandasLFApplier
 
-lfs = worker_lfs_pos + worker_lfs_neg
-
-applier = PandasLFApplier(lfs)
+applier = PandasLFApplier(worker_lfs)
 L_train = applier.apply(df_train)
 L_dev = applier.apply(df_dev)
+
+# %% [markdown]
+# Note that because our dev set is so small and our LFs are relatively sparse, many LFs will appear to have zero coverage.
+# Fortunately, our label model learns weights for LFs based on their coverage on the training set, which is generally much larger.
 
 # %%
 from snorkel.labeling import LFAnalysis
 
-LFAnalysis(L_dev, lfs).lf_summary(Y_dev).head()
+LFAnalysis(L_dev, worker_lfs).lf_summary(Y_dev).sample(5)
 
 # %% [markdown]
-# So the crowd labels are quite good! But how much of our dev and training
+# So the crowd labels in general are quite good! But how much of our dev and training
 # sets do they cover?
 
 # %%
-print("Training set coverage:", LFAnalysis(L_train).label_coverage())
-print("Dev set coverage:", LFAnalysis(L_dev).label_coverage())
+print(f"Training set coverage: {LFAnalysis(L_train).label_coverage(): 0.3f}")
+print(f"Dev set coverage: {LFAnalysis(L_dev).label_coverage(): 0.3f}")
 
 # %% [markdown]
 # ### Additional labeling functions
 #
-# To improve coverage of the training set, we can mix the crowd worker labeling functions with labeling
+# To improve coverage of the training set, we can mix the crowdworker labeling functions with labeling
 # functions of other types.
 # For example, we can use [TextBlob](https://textblob.readthedocs.io/en/dev/index.html), a tool that provides a pretrained sentiment analyzer. We run TextBlob on our tweets and create some simple LFs that threshold its polarity score, similar to what we did in the spam_tutorial.
 
@@ -171,14 +149,12 @@ from snorkel.preprocess import preprocessor
 from textblob import TextBlob
 
 
-@preprocessor()
+@preprocessor(memoize=True)
 def textblob_polarity(x):
     scores = TextBlob(x.tweet_text)
     x.polarity = scores.polarity
     return x
 
-
-textblob_polarity.memoize = True
 
 # Label high polarity tweets as positive.
 @labeling_function(pre=[textblob_polarity])
@@ -203,7 +179,7 @@ def polarity_negative_2(x):
 
 # %%
 text_lfs = [polarity_positive, polarity_negative, polarity_negative_2]
-lfs = text_lfs + worker_lfs_pos + worker_lfs_neg
+lfs = text_lfs + worker_lfs
 
 applier = PandasLFApplier(lfs)
 L_train = applier.apply(df_train)
@@ -219,8 +195,8 @@ LFAnalysis(L_dev, lfs).lf_summary(Y_dev).head()
 # to denoise and combine them.
 
 # %%
-print("Training set coverage:", LFAnalysis(L_train).label_coverage())
-print("Dev set coverage:", LFAnalysis(L_dev).label_coverage())
+print(f"Training set coverage: {LFAnalysis(L_train).label_coverage(): 0.3f}")
+print(f"Dev set coverage: {LFAnalysis(L_dev).label_coverage(): 0.3f}")
 
 # %% [markdown]
 # ## Train LabelModel And Generate Probabilistic Labels
@@ -246,16 +222,14 @@ acc = metric_score(Y_dev, Y_dev_pred, probs=None, metric="accuracy")
 print(f"LabelModel Accuracy: {acc:.3f}")
 
 # %% [markdown]
-# Look at that, we get very high accuracy on the development set.
-# This is due to the abundance of high quality crowd worker labels.
+# We see that we get very high accuracy on the development set.
+# This is due to the abundance of high quality crowdworker labels.
 # **Since we don't have these high quality crowdsourcing labels for the
 # test set or new incoming examples, we can't use the LabelModel reliably
 # at inference time.**
 # In order to run inference on new incoming examples, we need to train a
 # discriminative model over the tweets themselves.
-# Let's generate a set of probabilistic labels for the training set.
-
-# %%
+# Let's generate a set of probabilistic labels for that training set.
 
 # %%
 Y_train_prob = label_model.predict_proba(L_train)
@@ -265,7 +239,9 @@ Y_train_prob = label_model.predict_proba(L_train)
 
 # %% [markdown]
 # ### Getting features from BERT
-# Since we have very limited training data, we cannot train a complex model like an LSTM with a lot of parameters. Instead, we use a pre-trained model, [BERT](https://github.com/google-research/bert), to generate embeddings for each our tweets, and treat the embedding values as features.
+# Since we have very limited training data, we cannot train a complex model like an LSTM with a lot of parameters. 
+# Instead, we use a pre-trained model, [BERT](https://github.com/google-research/bert), to generate embeddings for each our tweets, and treat the embedding values as features.
+# This may take 5-10 minutes on a CPU, as the BERT model is very large.
 
 # %%
 import numpy as np
@@ -295,15 +271,16 @@ from sklearn.linear_model import LogisticRegression
 sklearn_model = LogisticRegression(solver="liblinear")
 sklearn_model.fit(train_vectors, probs_to_preds(Y_train_prob))
 
+# %%
 print(f"Accuracy of trained model: {sklearn_model.score(test_vectors, Y_test)}")
 
 # %% [markdown]
-# We now have a model with accuracy not much lower than the LabelModel, but with the advantage of being faster and cheaper than crowdsourcing, and applicable to all future examples.
+# We now have a trained model that can be applied to future examples without requiring crowdsourced labels, and with accuracy not much lower than the `LabelModel` that _does_ have access to crowdsourced labels! 
 
 # %% [markdown]
 # ## Summary
 #
 # In this tutorial, we accomplished the following:
-# * We showed how Snorkel can handle crowdsourced labels, combining them with other programmatic LFs to improve coverage.
-# * We showed how the LabelModel learns to combine inputs from crowd workers and other LFs by appropriately weighting them to generate high quality probabilistic labels.
-# * We showed that a classifier trained on the combined labels can achieve a fairly high accuracy while also generalizing to new, unseen examples.
+# * We demonstrated how to combine crowdsourced labels with other programmatic LFs to improve coverage.
+# * We used the `LabelModel` to learn how to combine inputs from crowdworkers and other LFs to generate high quality probabilistic labels.
+# * We used our probabilistic labels to train a classifier for making predictions on new, unseen examples.
