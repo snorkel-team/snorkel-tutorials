7c0840aa44755de8d01c7ac82d2e9804b544ab72
Alex Ratner <ajratner@gmail.com>
2019-08-10 16:48:45 -0700

    Editing down code to minimal form as suggested by HE

--- intro/intro_tutorial.py
+++ intro/intro_tutorial.py
@@ -84,20 +84,7 @@ if os.path.basename(os.getcwd()) == "snorkel-tutorials":
 from utils import load_spam_dataset
 
 # Load data- for details see the spam tutorial
-df_train, df_dev, df_valid, df_test = load_spam_dataset()
-Y_dev = df_dev["label"].values
-Y_valid = df_valid["label"].values
-Y_test = df_test["label"].values
-
-# %% [markdown]
-# Let's quickly look at a few data points, which have been loaded in as Pandas DataFrames:
-
-# %%
-import pandas as pd
-
-# Don't truncate text fields in the display
-pd.set_option("display.max_colwidth", 0)
-df_dev.sample(5, random_state=3)
+df_train, *_ = load_spam_dataset()
 
 # %% [markdown]
 # Each row is one comment consisting of text, author, and date values, as well as an integer id for which YouTube video the comment corresponds to.
@@ -110,53 +97,36 @@ df_dev.sample(5, random_state=3)
 # The basic idea is simple: **a labeling function is a function that labels some subset of the training dataset**.
 # That is, each labeling function either outputs `SPAM`, `HAM`, or `ABSTAIN`:
 
-# %%
-HAM = 0
-SPAM = 1
-ABSTAIN = -1
-
 # %% [markdown]
 # Labeling functions can be used to represent many heuristic strategies for labeling data.
 # **The key idea is that labeling functions do not need to be perfectly accurate**, as Snorkel will automatically estimate their accuracies and correlations, and then reweight and combine their output labels, leading to high-quality training labels.
 #
-# As a starting example, labeling functions can be based on heuristically **matching keywords**:
+# As a starting example, labeling functions can be based on **matching keywords**, using **regular expressions**, leveraging arbitrary **heuristics**, using **third-party models**, and much more- anything that can be expressed as a function that labels!
 
 # %%
 from snorkel.labeling.lf import labeling_function
+import re
+from textblob import TextBlob
+
+# Define the label mappings for convenience
+ABSTAIN = -1
+HAM = 0
+SPAM = 1
 
 @labeling_function()
 def lf_keyword_my(x):
     """Many spam comments talk about 'my channel', 'my video', etc."""
     return SPAM if "my" in x.text.lower() else ABSTAIN
 
-
-# %% [markdown]
-# Labeling functions can also use **regular expressions**:
-
-# %%
-import re
-
 @labeling_function()
 def lf_regex_check_out(x):
     """Spam comments say 'check out my video', 'check it out', etc."""
     return SPAM if re.search(r"check.*out", x.text, flags=re.I) else ABSTAIN
 
-
-# %% [markdown]
-# Labeling functions can of course flexibly express a range of other **heuristics**:
-
-# %%
 @labeling_function()
 def lf_short_comment(x):
     """Ham comments are often short, such as 'cool video!'"""
     return HAM if len(x.text.split()) < 5 else ABSTAIN
-
-
-# %% [markdown]
-# Labeling functions can also use **third-party models**:
-
-# %%
-from textblob import TextBlob
 
 @labeling_function()
 def lf_textblob_polarity(x):
@@ -167,16 +137,17 @@ def lf_textblob_polarity(x):
     sentiment = TextBlob(x.text).sentiment
     return HAM if sentiment.polarity > 0.3 else ABSTAIN
 
+lfs = [
+    lf_keyword_my,
+    lf_regex_check_out,
+    lf_short_comment,
+    lf_textblob_polarity
+]
 
 # %% [markdown]
 # For many more types of labeling functions---including over data modalities beyond text---see the other [tutorials](https://github.com/snorkel-team/snorkel-tutorials) and examples at [snorkel.org](http://snorkel.org).
 # In general the process of developing labeling functions is, like any other development process, an iterative one that takes time- but that, in many cases, can be orders-of-magnitude faster that hand-labeling training data.
 # For more detail on the process of developing labeling functions and other training data operators in Snorkel, see the [full version of this tutorial](https://github.com/snorkel-team/snorkel-tutorials/tree/master/spam).
-#
-# For now, we proceed by loading the above labeling functions plus some additional similar ones from a utility file:
-
-# %%
-from lfs import lfs
 
 # %% [markdown]
 # ## STEP 2: Combining & Cleaning the Labels
@@ -184,50 +155,27 @@ from lfs import lfs
 # Our next step is to apply the labeling functions we wrote to the unlabeled training data; we do this using the `LFApplier` corresponding to our base data class (in this case, the `PandasLFApplier`):
 
 # %%
-from snorkel.labeling.apply import PandasLFApplier
+from snorkel.labeling.apply.pandas import PandasLFApplier
 
 applier = PandasLFApplier(lfs)
 L_train = applier.apply(df_train)
-L_dev = applier.apply(df_dev)
-L_test = applier.apply(df_test)
 
 # %% [markdown]
 # The result of applying the labeling functions (LFs) to the data is (for each split of the data) a _label matrix_ with rows corresponding to data points, and columns corresponding to LFs.
 # We can take a look at some statistics of the LFs on the dev set:
 
 # %%
-from snorkel.labeling.analysis import LFAnalysis
-LFAnalysis(L_dev, lfs).lf_summary(Y=Y_dev)
+from snorkel.labeling.model.label_model import LabelModel
 
-# %% [markdown]
-# Above, we see that the LFs indeed have varying accuracies, conflict with each other on non-negligible portions of the training set, and also each cover only some subset of the training set.
-# Next, we use Snorkel's `LabelModel` to automatically learn the accuracies* and correlations of the LFs, and reweight and combine their outputs into a final set of _probabilistic_ training labels.
-#
-# *_Note that the "Empirical Accuracies" in the table above are computed over a very small labeled development set that we looked at while writing the LFs- thus, it is a very biased estimate of their accuracies (this is, for example, why so many of them have 100% accuracy according to this estimate...).  Thus, we rely on a [novel unsupervised generative modeling technique](https://arxiv.org/abs/1605.07723) in Snorkel to learn their accuracies in a provably convergent way over the much larger unlabeled training set. For more technical details of this overall approach, see our [NeurIPS 2016](https://arxiv.org/abs/1605.07723) and [AAAI 2019](https://arxiv.org/abs/1810.02840) papers._
-
-# %%
-from snorkel.labeling.model import LabelModel
-
-# Training the LabelModel
 label_model = LabelModel(cardinality=2, verbose=True)
 label_model.fit(L_train, n_epochs=500, log_freq=50, seed=123)
-
-# Getting the probabilistic labels for the training set from the LabelModel
-Y_probs_train = label_model.predict_proba(L=L_train)
-
-# Computing the accuracy of the LabelModel
-label_model_acc = label_model.score(L_test, Y_test)["accuracy"]
-print(f"{'Label Model Accuracy:':<25} {label_model_acc * 100:.1f}%")
+Y_preds_train = label_model.predict(L=L_train)
 
 # %% [markdown]
 # Note that above, we have applied the `LabelModel` to the test set, and see that we get an accuracy score of approximately $85\%$.
 # In many Snorkel applications, it is not possible to apply the labeling functions (and therefore the `LabelModel`) at test time, for example due to the labeling functions using features not available at test time, or being overly slow to execute (for an example of this, see the [cross-modal tutorial](), and other examples at [snorkel.org]()).
 #
 # Here, we _can_ apply the `LabelModel` to the test set, but it leaves a lot to be desired- in part because the labeling functions leave a large portion of the dataset unlabeled:
-
-# %%
-from utils import plot_label_frequency
-plot_label_frequency(L_train)
 
 # %% [markdown]
 # Here is where the final step of the pipeline comes in handy- we will now use the probabilistic training labels to train a machine learning model which will generalize beyond---and outperform---the labeling functions.
@@ -244,64 +192,13 @@ plot_label_frequency(L_train)
 
 # %%
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
 
-vectorizer = CountVectorizer(ngram_range=(1, 2))
-X_train = vectorizer.fit_transform([row.text for i, row in df_train.iterrows()])
-X_valid = vectorizer.transform([row.text for i, row in df_valid.iterrows()])
-X_test = vectorizer.transform([row.text for i, row in df_test.iterrows()])
+text_train = [row.text for i, row in df_train.iterrows()]
+X_train = CountVectorizer(ngram_range=(1, 2)).fit_transform(text_train)
 
-# %% [markdown]
-# **TODO: This step will be removed!**
-
-# %%
-mask = L_train.sum(axis=1) != ABSTAIN * len(lfs)
-X_train = X_train[mask, :]
-Y_probs_train = Y_probs_train[mask]
-
-# %% [markdown]
-# Now, we'll use Keras, a popular high-level API for building models in TensorFlow, to build a simple logistic regression classifier.
-# We compile it with a `categorical_crossentropy` loss so that it can handle probabilistic labels instead of integer labels.
-# Using a _noise-aware loss_ &mdash; one that uses probabilistic labels &mdash; for our discriminative model lets
-# us take full advantage of the label model's learning procedure (see our [NeurIPS 2016 paper](https://arxiv.org/abs/1605.07723)).
-# We use the common settings of an `Adam` optimizer and early stopping (evaluating the model on the validation set after each epoch and reloading the weights from when it achieved the best score).
-# For more information on Keras, see the [Keras documentation](https://keras.io/).
-
-# %%
-from snorkel.analysis.utils import probs_to_preds, preds_to_probs
-from snorkel.analysis.metrics import metric_score
-import tensorflow as tf
-
-# Our model is a simple linear layer mapping from feature
-# vectors to the number of labels in our problem (2).
-keras_model = tf.keras.Sequential()
-keras_model.add(
-    tf.keras.layers.Dense(
-        2,
-        input_dim=X_train.shape[1],
-        activation=tf.nn.softmax,
-        kernel_regularizer=tf.keras.regularizers.l2(0.01),
-    )
-)
-optimizer = tf.keras.optimizers.Adam(lr=0.001)
-keras_model.compile(
-    optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy"]
-)
-
-early_stopping = tf.keras.callbacks.EarlyStopping(
-    monitor="val_acc", patience=10, verbose=1, restore_best_weights=True
-)
-
-keras_model.fit(
-    X_train,
-    Y_probs_train,
-    validation_data=(X_valid, preds_to_probs(Y_valid, 2)),
-    callbacks=[early_stopping],
-    epochs=20,
-    verbose=0,
-)
-
-Y_preds_test = probs_to_preds(keras_model.predict(X_test))
-print(f"Test Accuracy: {metric_score(Y_test, Y_preds_test, metric='accuracy')}")
+clf = LogisticRegression()
+clf.fit(X=X_train, y=Y_preds_train)
 
 # %% [markdown]
 # **We observe an additional boost in accuracy over the `LabelModel` by multiple points!
