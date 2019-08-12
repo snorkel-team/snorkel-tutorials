0cba4762bac3183373967e9c4950dd5d0cd10995
Braden Hancock <hancock.braden@gmail.com>
2019-08-12 16:13:57 -0700

    [EASY] Update path to snorkel to reflect ownership transfer (#82)
    * Update path to snorkel to reflect ownership transfer
    
    * Restore path to snorkel-superglue on HazyResearch

--- crowdsourcing/download-data.sh
+++ crowdsourcing/download-data.sh
@@ -21,6 +21,6 @@ done
 if [ "$RELOAD" = "true" ]; then
     if [ -d "data/" ]; then rm -Rf "data/"; fi
     mkdir -p data
-    wget https://raw.githubusercontent.com/HazyResearch/snorkel/master/tutorials/crowdsourcing/data/weather-non-agg-DFE.csv -P data
+    wget https://raw.githubusercontent.com/snorkel-team/snorkel/master/tutorials/crowdsourcing/data/weather-non-agg-DFE.csv -P data
     wget https://d1p17r2m4rzlbo.cloudfront.net/wp-content/uploads/2016/03/weather-evaluated-agg-DFE.csv -P data
 fi
