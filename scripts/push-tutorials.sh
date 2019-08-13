880e0a1bee5d2aca0565db3a7bbab3e92208f8aa
Henry Ehrenberg <henry.ehrenberg@outlook.com>
2019-08-13 18:39:40 +0000

    Fix commands

--- scripts/push-tutorials.sh
+++ scripts/push-tutorials.sh
@@ -10,19 +10,20 @@ checkout_website() {
 }
 
 build_tutorials() {
-  rm -rf build website/_use_cases _site/use-cases/
+  rm -rf build website/_use_cases
   tox -e markdown
+  mkdir -p website/_use_cases
   mv -f build/*.md website/_use_cases
-  mv -f build/*/ website/_site/use-cases/
 }
 
 push_tutorials() {
   cd website
-  git add _use_cases _site/use-cases
+  git add _use_cases
   git commit -m "[DEPLOY $TRAVIS_BUILD_NUMBER] Update tutorials"
   git push https://$GITHUB_TOKEN@github.com/snorkel-team/website.git master -f > /dev/null 2>&1
 }
 
+echo "Deploying tutorials"
 setup_git
 checkout_website
 build_tutorials
