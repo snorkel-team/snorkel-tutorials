10fb4772058ebf632e24a8e1183f23bc1e0f5087
Braden Hancock <hancock.braden@gmail.com>
2019-08-14 22:19:09 -0700

    Update to jupytext percent format

--- visual_relation/visual_relation_tutorial.py
+++ visual_relation/visual_relation_tutorial.py
@@ -1,19 +1,4 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.4'
-#       jupytext_version: 1.2.0
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
+# %% [markdown]
 # # Visual Relationship Detection
 #
 # In this tutorial, we focus on the task of classifying visual relationships between objects in an image. For any given image, there might be many such relationships, defined formally as a `subject <predictate> object` (e.g. `person <riding> bike`). As an example, in the relationship `man riding bicycle`), "man" and "bicycle" are the subject and object, respectively, and "riding" is the relationship predicate.
@@ -24,13 +9,13 @@
 #
 # For the purpose of this tutorial, we operate over the [Visual Relationship Detection (VRD) dataset](https://cs.stanford.edu/people/ranjaykrishna/vrd/) and focus on action relationships. We define our classification task as **identifying which of three relationships holds between the objects represented by a pair of bounding boxes.**
 
-# +
+# %%
 import os
 
 if os.path.basename(os.getcwd()) == "visual_relation":
     os.chdir("..")
-# -
 
+# %% [markdown]
 # ### 1. Load Dataset
 # We load the VRD dataset and filter images with at least one action predicate in it, since these are more difficult to classify than geometric relationships like `above` or `next to`. We load the train, valid, and test sets as Pandas `DataFrame` objects with the following fields:
 # - `label`: The relationship between the objects. 0: `RIDE`, 1: `CARRY`, 2: `OTHER` action predicates
@@ -40,11 +25,12 @@ if os.path.basename(os.getcwd()) == "visual_relation":
 # - `subject_bbox`: coordinates of the bounding box for the object `[ymin, ymax, xmin, xmax]`
 # - `subject_category`: category of the subject
 
+# %% [markdown]
 # If you are running this notebook for the first time, it will take ~15 mins to download all the required sample data.
 #
 # The sampled version of the dataset **uses the same 26 examples across the train, dev, and test sets. This setting is meant to demonstrate how Snorkel works with this task, not to demonstrate performance.**
 
-# +
+# %%
 from visual_relation.utils import load_vrd_data
 
 # setting sample=False will take ~3 hours to run (downloads full VRD dataset)
@@ -55,23 +41,26 @@ df_train, df_valid, df_test = load_vrd_data(sample, is_travis)
 print("Train Relationships: ", len(df_train))
 print("Dev Relationships: ", len(df_valid))
 print("Test Relationships: ", len(df_test))
-# -
 
+# %% [markdown]
 # Note that the training `DataFrame` will have a labels field with all -1s. This denotes the lack of labels for that particular dataset. In this tutorial, we will assign probabilistic labels to the training set by writing labeling functions over attributes of the subject and objects!
 
+# %% [markdown]
 # ## 2. Writing Labeling Functions
 # We now write labeling functions to detect what relationship exists between pairs of bounding boxes. To do so, we can encode various intuitions into the labeling functions:
 # * _Categorical_ intution: knowledge about the categories of subjects and objects usually involved in these relationships (e.g., `person` is usually the subject for predicates like `ride` and `carry`)
 # * _Spatial_ intuition: knowledge about the relative positions of the subject and objects (e.g., subject is usually higher than the object for the predicate `ride`)
 
+# %%
 RIDE = 0
 CARRY = 1
 OTHER = 2
 ABSTAIN = -1
 
+# %% [markdown]
 # We begin with labeling functions that encode categorical intuition: we use knowledge about common subject-object category pairs that are common for `RIDE` and `CARRY` and also knowledge about what subjects or objects are unlikely to be involved in the two relationships.
 
-# +
+# %%
 from snorkel.labeling import labeling_function
 
 # Category-based LFs
@@ -114,16 +103,16 @@ def lf_not_person(x):
     return ABSTAIN
 
 
-# -
-
+# %% [markdown]
 # We now encode our spatial intuition, which includes measuring the distance between the bounding boxes and comparing their relative areas.
 
+# %%
 YMIN = 0
 YMAX = 1
 XMIN = 2
 XMAX = 3
 
-# +
+# %%
 import numpy as np
 
 # Distance-based LFs
@@ -153,11 +142,10 @@ def lf_area(x):
     return ABSTAIN
 
 
-# -
-
+# %% [markdown]
 # Note that the labeling functions have varying empirical accuracies and coverages. Due to class imbalance in our chosen relationships, labeling functions that label the `OTHER` class have higher coverage than labeling functions for `RIDE` or `CARRY`. This reflects the distribution of classes in the dataset as well.
 
-# +
+# %%
 from snorkel.labeling import PandasLFApplier
 
 lfs = [
@@ -174,33 +162,36 @@ applier = PandasLFApplier(lfs)
 L_train = applier.apply(df_train)
 L_valid = applier.apply(df_valid)
 
-# +
+# %%
 from snorkel.labeling import LFAnalysis
 
 Y_valid = df_valid.label.values
 LFAnalysis(L_valid, lfs).lf_summary(Y_valid)
-# -
 
+# %% [markdown]
 # ## 3. Train Label Model
 # We now train a multi-class `LabelModel` to assign training labels to the unalabeled training set.
 
-# +
+# %%
 from snorkel.labeling import LabelModel
 
 label_model = LabelModel(cardinality=3, verbose=True)
 label_model.fit(L_train, seed=123, lr=0.01, log_freq=10, n_epochs=100)
-# -
 
+# %% [markdown]
 # We use [F1](https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html) Micro average for the multiclass setting, which calculates metrics globally across classes, by counting the total true positives, false negatives and false positives.
 
+# %%
 label_model.score(L_valid, Y_valid, metrics=["f1_micro"])
 
+# %% [markdown]
 # ## 4. Train a Classifier
 # You can then use these training labels to train any standard discriminative model, such as [an off-the-shelf ResNet](https://github.com/KaimingHe/deep-residual-networks), which should learn to generalize beyond the LF's we've developed!
 
+# %% [markdown]
 # #### Create DataLoaders for Classifier
 
-# +
+# %%
 from snorkel.classification import DictDataLoader
 from visual_relation.model import SceneGraphDataset, create_model
 
@@ -222,21 +213,21 @@ dl_valid = DictDataLoader(
     batch_size=16,
     shuffle=False,
 )
-# -
 
+# %% [markdown]
 # #### Define Model Architecture
 
-# +
+# %%
 import torchvision.models as models
 
 # initialize pretrained feature extractor
 cnn = models.resnet18(pretrained=True)
 model = create_model(cnn)
-# -
 
+# %% [markdown]
 # ### Train and Evaluate Model
 
-# +
+# %%
 from snorkel.classification import Trainer
 
 trainer = Trainer(
@@ -246,8 +237,9 @@ trainer = Trainer(
     checkpointer_config={"checkpoint_dir": "checkpoint"},
 )
 trainer.fit(model, [dl_train])
-# -
 
+# %%
 model.score([dl_valid])
 
+# %% [markdown]
 # We have successfully trained a visual relationship detection model! Using categorical and spatial intuition about how objects in a visual relationship interact with each other, we are able to assign high quality training labels to object pairs in the VRD dataset in a multi-class classification setting.
