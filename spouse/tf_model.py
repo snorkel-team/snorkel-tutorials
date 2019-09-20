93c1a154b7313b64fcf1e47f849208c761796149
Manas Joglekar <brahmaneya@gmail.com>
2019-09-20 11:54:54 -0700

    Use Bi-LSTM model for Spouse Demo (#150)
    * BiLSTM model
    
    * tox fix
    
    * Simplfy token commands

--- spouse/tf_model.py
+++ spouse/tf_model.py
@@ -2,56 +2,68 @@
 from typing import Tuple
 import numpy as np
 import pandas as pd
+
 import tensorflow as tf
+from tensorflow.keras.layers import (
+    Bidirectional,
+    Concatenate,
+    Dense,
+    Embedding,
+    Input,
+    LSTM,
+)
 
 
 def get_feature_arrays(df: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Get np arrays of upto max_length tokens and person idxs."""
+    bet = df.between_tokens
+    left = df.apply(lambda c: c.tokens[: c.person1_word_idx[0]][-4:-1], axis=1)
+    right = df.person2_right_tokens
 
     def pad_or_truncate(l, max_length=40):
         return l[:max_length] + [""] * (max_length - len(l))
 
-    tokens = np.array(list(map(pad_or_truncate, df.tokens)))
-    idx1 = np.array(list(map(list, df.person1_word_idx)))
-    idx2 = np.array(list(map(list, df.person2_word_idx)))
-    return tokens, idx1, idx2
+    left_tokens = np.array(list(map(pad_or_truncate, left)))
+    bet_tokens = np.array(list(map(pad_or_truncate, bet)))
+    right_tokens = np.array(list(map(pad_or_truncate, right)))
+    return left_tokens, bet_tokens, right_tokens
+
+
+def bilstm(
+    tokens: tf.Tensor,
+    rnn_state_size: int = 64,
+    num_buckets: int = 40000,
+    embed_dim: int = 36,
+):
+    ids = tf.strings.to_hash_bucket(tokens, num_buckets)
+    embedded_input = Embedding(num_buckets, embed_dim)(ids)
+    return Bidirectional(LSTM(rnn_state_size, activation=tf.nn.relu))(
+        embedded_input, mask=tf.strings.length(tokens)
+    )
 
 
 def get_model(
-    rnn_state_size: int = 64,
-    num_hashes: int = 3,
-    num_buckets: int = 40000,
-    embed_dim: int = 12,
+    rnn_state_size: int = 64, num_buckets: int = 40000, embed_dim: int = 12
 ) -> tf.keras.Model:
     """
     Return LSTM model for predicting label probabilities.
-
     Args:
         rnn_state_size: LSTM state size.
-        num_hashes: Number of distinct hash functions to use.
         num_buckets: Number of buckets to hash strings to integers.
         embed_dim: Size of token embeddings.
-
     Returns:
         model: A compiled LSTM model.
     """
-    tokens_ph = tf.keras.layers.Input((None,), dtype="string")
-    idx1_ph = tf.keras.layers.Input((2,), dtype="int64")
-    idx2_ph = tf.keras.layers.Input((2,), dtype="int64")
-    num_words = tf.shape(tokens_ph)[1]
-    idx1_start = tf.one_hot(idx1_ph[:, 0], num_words)
-    idx1_end = tf.one_hot(idx1_ph[:, 1], num_words)
-    idx2_start = tf.one_hot(idx2_ph[:, 0], num_words)
-    idx2_end = tf.one_hot(idx2_ph[:, 1], num_words)
-    embeddings = [tf.stack([idx1_start, idx1_end, idx2_start, idx2_end], 2)]
-    for i in range(num_hashes):
-        ids = tf.strings.to_hash_bucket(str(i) + tokens_ph, num_buckets)
-        embeddings.append(tf.keras.layers.Embedding(num_buckets, embed_dim)(ids))
-    embedded_input = tf.concat(embeddings, 2)
-    output = tf.keras.layers.LSTM(rnn_state_size, activation=tf.nn.relu)(
-        embedded_input, mask=tf.strings.length(tokens_ph)
-    )
-    probabilities = tf.keras.layers.Dense(2, activation=tf.nn.softmax)(output)
-    model = tf.keras.Model(inputs=[tokens_ph, idx1_ph, idx2_ph], outputs=probabilities)
+    left_ph = Input((None,), dtype="string")
+    bet_ph = Input((None,), dtype="string")
+    right_ph = Input((None,), dtype="string")
+    left_embs = bilstm(left_ph, rnn_state_size, num_buckets, embed_dim)
+    bet_embs = bilstm(bet_ph, rnn_state_size, num_buckets, embed_dim)
+    right_embs = bilstm(right_ph, rnn_state_size, num_buckets, embed_dim)
+    layer = Concatenate(1)([left_embs, bet_embs, right_embs])
+    layer = Dense(64, activation=tf.nn.relu)(layer)
+    layer = Dense(32, activation=tf.nn.relu)(layer)
+    probabilities = Dense(2, activation=tf.nn.softmax)(layer)
+    model = tf.keras.Model(inputs=[bet_ph, left_ph, right_ph], outputs=probabilities)
     model.compile(tf.train.AdagradOptimizer(0.1), "categorical_crossentropy")
     return model
