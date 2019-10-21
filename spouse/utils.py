c42b7b8bbc455a6f5e142f385b6ebb405fdccfd6
Andrew Kittredge <andrew@calcbench.com>
2019-10-21 13:20:42 -0400

    remove dependency on bash, use python to do the downloading

--- spouse/utils.py
+++ spouse/utils.py
@@ -4,8 +4,12 @@ import subprocess
 from typing import Tuple
 
 import numpy as np
+import shutil
 
 import pandas as pd
+
+import urllib
+import zipfile
 
 IS_TEST = os.environ.get("TRAVIS") == "true" or os.environ.get("IS_TEST") == "true"
 
@@ -19,11 +23,7 @@ def load_data() -> Tuple[
         df_train: Training set data points dataframe.
         df_test, Y_test: Test set data points dataframe and 1D labels ndarray.
     """
-    try:
-        subprocess.run(["bash", "download_data.sh"], check=True, stderr=subprocess.PIPE)
-    except subprocess.CalledProcessError as e:
-        print(e.stderr.decode())
-        raise e
+    download_files()
     with open(os.path.join("data", "dev_data.pkl"), "rb") as f:
         df_dev = pickle.load(f)
         Y_dev = pickle.load(f)
@@ -46,3 +46,25 @@ def load_data() -> Tuple[
 
 def get_n_epochs() -> int:
     return 3 if IS_TEST else 30
+
+
+DATA_URL="https://www.dropbox.com/s/jmrvyaqew4zp9cy/spouse_data.zip?dl=1"
+FILES=( "train_data.pkl" "dev_data.pkl" "test_data.pkl" "dbpedia.pkl" )
+
+def download_files():
+    # Check that we are running from the right directory.
+    if os.path.split(os.getcwd())[1] != 'spouse':
+        raise Exception("Script must be run from spouse directory")
+    reload = False
+    for filename in FILES:
+        if not os.path.exists(filename):
+            reload = True
+    if reload:
+        if os.path.exists("data/"):
+            shutil.rmtree("data/")
+            os.mkdir("data")
+        urllib.request.urlretrieve(DATA_URL, "data.zip")       
+        with zipfile.ZipFile("data.zip", "r") as zip_ref:
+            zip_ref.extractall("data")
+        os.remove("data.zip")
+        
