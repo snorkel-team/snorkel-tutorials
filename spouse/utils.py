bf5663763b539d1700d0339e85acd183c0b49d08
Andrew Kittredge <andrew@calcbench.com>
2019-12-10 11:33:25 -0500

    replacing bash dependency

--- spouse/utils.py
+++ spouse/utils.py
@@ -2,17 +2,22 @@ import os
 import pickle
 import subprocess
 from typing import Tuple
+import sys
 
 import numpy as np
 import shutil
 
 import pandas as pd
 
-import urllib
-import zipfile
+
+
+sys.path.insert(0, os.path.split(os.path.dirname(__file__))[0]) # so we can import from utils
+from snorkle_example_utils.download_files import download_files
 
 IS_TEST = os.environ.get("TRAVIS") == "true" or os.environ.get("IS_TEST") == "true"
-
+DATA_URL="https://www.dropbox.com/s/jmrvyaqew4zp9cy/spouse_data.zip?dl=1"
+FILES=( "train_data.pkl", "dev_data.pkl", "test_data.pkl", "dbpedia.pkl" )
+DIRECTORY = "spouse"
 
 def load_data() -> Tuple[
     Tuple[pd.DataFrame, np.ndarray], pd.DataFrame, Tuple[pd.DataFrame, np.ndarray]
@@ -23,7 +28,8 @@ def load_data() -> Tuple[
         df_train: Training set data points dataframe.
         df_test, Y_test: Test set data points dataframe and 1D labels ndarray.
     """
-    download_files()
+    
+    download_files(FILES, DATA_URL, DIRECTORY)
     with open(os.path.join("data", "dev_data.pkl"), "rb") as f:
         df_dev = pickle.load(f)
         Y_dev = pickle.load(f)
@@ -46,25 +52,3 @@ def load_data() -> Tuple[
 
 def get_n_epochs() -> int:
     return 3 if IS_TEST else 30
-
-
-DATA_URL="https://www.dropbox.com/s/jmrvyaqew4zp9cy/spouse_data.zip?dl=1"
-FILES=( "train_data.pkl" "dev_data.pkl" "test_data.pkl" "dbpedia.pkl" )
-
-def download_files():
-    # Check that we are running from the right directory.
-    if os.path.split(os.getcwd())[1] != 'spouse':
-        raise Exception("Script must be run from spouse directory")
-    reload = False
-    for filename in FILES:
-        if not os.path.exists(filename):
-            reload = True
-    if reload:
-        if os.path.exists("data/"):
-            shutil.rmtree("data/")
-            os.mkdir("data")
-        urllib.request.urlretrieve(DATA_URL, "data.zip")       
-        with zipfile.ZipFile("data.zip", "r") as zip_ref:
-            zip_ref.extractall("data")
-        os.remove("data.zip")
-        
