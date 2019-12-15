2c8fb8443ebbaa68e0715f5073f84758a4ae2384
jdunnmon <jdunnmon@gmail.com>
2019-12-15 03:39:56 -0800

    refactored ludwig spam tutorial

---
+++ ludwig/utils.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def indices_to_one_hot(data):
+    """Convert an iterable of indices to one-hot encoded labels."""
+    nb_classes = len(np.unique(data))
+    targets = np.array(data).reshape(-1)
+    return np.eye(nb_classes)[targets]
