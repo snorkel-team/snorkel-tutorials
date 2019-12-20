eca12aee7bc1db873a4a99a28750de879601bc62
Jared Dunnmon <jdunnmon@gmail.com>
2019-12-19 18:38:10 -0800

    restored spam

--- spam/utils.py
+++ spam/utils.py
@@ -148,10 +148,3 @@ def get_pytorch_mlp(hidden_dim, num_layers):
     for _ in range(num_layers):
         layers.extend([nn.Linear(hidden_dim, hidden_dim), nn.ReLU()])
     return nn.Sequential(*layers)
-
-
-def indices_to_one_hot(data):
-    """Convert an iterable of indices to one-hot encoded labels."""
-    nb_classes = len(np.unique(data))
-    targets = np.array(data).reshape(-1)
-    return np.eye(nb_classes)[targets]
